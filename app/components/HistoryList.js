8634a68493f09b7951685ff713f43571fcff0895
JonnyLinja <jonny.linja@gmail.com>
2017-05-17 00:16:15 -0400

    Moved history rest to its own component

--- app/components/HistoryList.js
+++ app/components/HistoryList.js
@@ -14,6 +14,7 @@ import {
 import EditHistorySetScreen from '../containers/EditHistorySetScreen';
 import SetDescription from './SetDescription';
 import SetData from './SetData';
+import SetRest from './SetRest';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import HistoryFilterBarScreen from '../containers/HistoryFilterBarScreen';
 import HistoryLoadingFooterScreen from '../containers/HistoryLoadingFooterScreen';
@@ -40,38 +41,12 @@ class HistoryList extends Component {
 		);
 	}
 
-	_renderHeader(item) {
-		return (
-			<View style={{flex: 1, flexDirection: 'column', marginTop: 15}}>
-				<View style={[styles.Shadow, {height: 1, backgroundColor: 'white', shadowRadius: 2, shadowOpacity: 1, shadowOffset: { height: 1, weight: 0 }}]}></View>
-				<TouchableHighlight onPress={ () => this.props.editHistorySet(item.setID) } activeOpacity={1}>
-					<View style={[styles.Shadow, {flexDirection:'column', justifyContent: 'flex-end', backgroundColor:'white'}]}>
-						<View style={{paddingLeft:10, paddingTop: 5, paddingBottom: 7}}>
-							<Text style={{color:item.row1Color}}>{item.row1}</Text>
-							<Text style={{color:item.row2Color}}>{item.row2}</Text>
-							<Text style={{color:item.row3Color}}>{item.row3}</Text>
-						</View>
-						<View style={{ paddingLeft: 5, paddingRight:17 }}>
-							<LegendBar />
-						</View>
-					</View>
-				</TouchableHighlight>
-			</View>
-		);
-	}
-
-	_renderFooter(item) {
-		return (
-			<View style={[styles.Shadow, {flex:1, flexDirection: 'row', alignItems:'stretch', backgroundColor:'white'}]}>
-				<Text style={{flex: 1, textAlign: 'center', marginTop: 15, color: 'gray', marginBottom: 15}}>{ item.rest }</Text>
-			</View>
-		);
-	}
-
 	_renderRow(item) {
 		switch (item.type) {
 			case "header":
-				return (<SetDescription item={item} onPressRow={() => this.props.editHistorySet(item.setID)} />);
+				return (<SetDescription item={item}
+							onPressRow={() => this.props.editHistorySet(item.setID)}
+						/>);
 			case "data":
 				return (<SetData item={item}
 							onPressRemove={() =>this.props.removeRep(item.setID, item.rep) }
@@ -79,7 +54,7 @@ class HistoryList extends Component {
 							onPressRow={() => this.props.editHistorySet(item.setID)}
 						/>);
 			case "footer":
-				return this._renderFooter(item);
+				return (<SetRest item={item} />);
 			default:
 				break;
 		}
---
+++ app/components/SetRest.js
@@ -0,0 +1,30 @@
+// app/components/SetRest.js
+
+import React, {PureComponent} from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+class SetRest extends PureComponent {
+
+    render() {
+        return (
+			<View style={[styles.Shadow, {flex:1, flexDirection: 'row', alignItems:'stretch', backgroundColor:'white'}]}>
+				<Text style={{flex: 1, textAlign: 'center', marginTop: 15, color: 'gray', marginBottom: 15}}>{ this.props.item.rest }</Text>
+			</View>
+		);
+    }
+
+}
+
+const styles = StyleSheet.create({
+    Shadow: {
+		shadowColor: "#000000",
+		shadowOpacity: 0.2,
+		shadowRadius: 2,
+		shadowOffset: {
+			height: 4,
+			width: 0
+		},
+	},
+});
+
+export default SetRest;

5141f62767643c27ff51da4082282aefe5b9794b
JonnyLinja <jonny.linja@gmail.com>
2017-07-16 03:30:18 -0400

    basic full screen mode

--- app/components/SetExpandedView.js
+++ app/components/SetExpandedView.js
@@ -1,5 +1,7 @@
 // app/components/SetExpandedView.js
 
+// TODO: variable columns, for now just hardcode them
+
 import React, {PureComponent} from 'react';
 import {
     View,
@@ -7,121 +9,133 @@ import {
     ScrollView,
     FlatList,
     Text,
-    Modal
+    Modal,
+    TouchableHighlight
 } from 'react-native';
 
 class SetExpandedView extends PureComponent {
 
-    _renderRow(item) {
+    _renderHeader() {
         return (
-            <View style={{flex: 1, flexDirection: 'row', width: 2000, height:200, backgroundColor: 'red'}}>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>1{item.key}</Text>
+            <View style={[styles.shadow, {height: 70, paddingTop: 35, paddingLeft: 15, marginBottom: 15}]}>
+                <TouchableHighlight onPress={() => this.props.closeModal()}>
+                    <Text>Close</Text>
+                </TouchableHighlight>
+            </View>
+        );
+    }
+
+    _renderStickyHeader() {
+        return (
+            <View style={{flexDirection: 'column', alignItems: 'stretch'}}>
+                    <View style={{flexDirection: 'row'}}>
+                        <View style={styles.headerLabel}><Text>REP</Text></View>
+                        <View style={styles.headerLabel}><Text>AVG</Text></View>
+                        <View style={styles.headerLabel}><Text>PKV</Text></View>
+                        <View style={styles.headerLabel}><Text>PKH</Text></View>
+                        <View style={styles.headerLabel}><Text>ROM</Text></View>
+                        <View style={styles.headerLabel}><Text>DUR</Text></View>
+                    </View>
+                    <View style={{flexDirection: 'row', marginBottom: 5}}>
+                        <View style={styles.headerLabel}><Text>#</Text></View>
+                        <View style={styles.headerLabel}><Text>m/s</Text></View>
+                        <View style={styles.headerLabel}><Text>m/s</Text></View>
+                        <View style={styles.headerLabel}><Text>%</Text></View>
+                        <View style={styles.headerLabel}><Text>mm</Text></View>
+                        <View style={styles.headerLabel}><Text>sec</Text></View>
+                    </View>
+                    <View style={styles.horizontalBorder}/>
+            </View>
+        );
+    }
+
+    _renderRow(item) {
+        var data = [];
+        let count = 0;
+        for (num of item.values) {
+            count++;
+            data.push(
+                <View key={item.key+" " +count} style={{flexDirection: "row"}}>
+                    <View style={styles.number}><Text>{num}</Text></View>
+                    <View style={styles.verticalBorder} />
                 </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>2{item.key}</Text>
+            );
+        }
+
+        if (item.key === 'rep1') {
+            var initialBorder = (<View style={[styles.horizontalBorder, {top: -1}]}/>);
+        } else {
+            var initialBorder = null;
+        }
+
+        return (
+            <View key={item.key} style={{flex: 1, flexDirection: 'column'}}>
+                {initialBorder}
+                <View style={{flex: 1, flexDirection: 'row'}}>
+                    <View style={styles.verticalBorder} />
+                    {data}
                 </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>3{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>4{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>5{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>6{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>7{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>8{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>9{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>10{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>11{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>12{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>13{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>14{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>15{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>16{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>17{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>18{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>19{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>20{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>21{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>22{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>23{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>24{item.key}</Text>
-                </View>
-                <View>
-                    <Text style={{marginHorizontal: 10}}>25{item.key}</Text>
-                </View>
+                <View style={styles.horizontalBorder}/>
             </View>
         );
     }
 
     render() {
-        let data = [{key: 'a'}, {key: 'b'}, {key: 'c'}, {key: 'd'}, {key: 'e'}, {key: 'f'}, {key: 'g'}, {key: 'h'}, {key: 'i'}, {key: 'j'}];
-
         return (
             <Modal
-                    animationType={"slide"}
+                    animationType={"fade"}
                     transparent={true}
                     visible={this.props.visible} >
 
-                    <ScrollView horizontal={true} style={{flex: 1, flexDirection: 'row'}}>
+                    <View style={{flex: 1, flexDirection: 'column', backgroundColor: 'white'}}>
+                        {this._renderHeader()}
+                        <ScrollView horizontal={true} style={{flex: 1, flexDirection: 'row'}}>
 
-                        <View style={{flex: 1, flexDirection: 'column'}}>
-                            <View>
-                                <Text>THIS IS THE HEADER THIS IS THE HEADER THIS IS THE HEADER THIS IS THE HEADER </Text>
+                            <View style={{flex: 1, flexDirection: 'column'}}>
+                                {this._renderStickyHeader()}
+                                <FlatList
+                                    data={this.props.rows}
+                                    renderItem={({item}) => this._renderRow(item)}
+                                />
                             </View>
-                            <FlatList
-                                style={{width: 2000, backgroundColor: 'green'}}
-                                data={data}
-                                renderItem={({item}) => this._renderRow(item)}
-                            />
-                        </View>
-                        
-                    </ScrollView>
-
+                            
+                        </ScrollView>
+                    </View>
             </Modal>
         );
     }
 
 }
+
+const styles = StyleSheet.create({
+    headerLabel: {
+        width: 80,
+        alignItems: 'center'
+    },
+    number: {
+        height: 44,
+        width: 79,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    horizontalBorder: {
+        backgroundColor: 'lightgray',
+        height: 1,
+    },
+	verticalBorder: {
+        width: 1,
+        backgroundColor: 'lightgray',
+    },
+    shadow: {
+		shadowColor: "#000000",
+		shadowOpacity: 0.2,
+		shadowRadius: 2,
+		shadowOffset: {
+			height: 4,
+			width: 0
+		},
+	},
+});
+
 
 export default SetExpandedView;
--- app/containers/HistorySetExpandedScreen.js
+++ app/containers/HistorySetExpandedScreen.js
@@ -5,16 +5,41 @@ import { connect } from 'react-redux';
 import SetExpandedView from '../components/SetExpandedView';
 import * as HistoryActionCreators from '../actions/HistoryActionCreators';
 import * as SetReducer from '../reducers/SetReducer';
+import * as RepDataMap from '../utility/RepDataMap';
 
 const mapStateToProps = (state) => {
 	if (state.history.expandedSetID !== null) {
 		var set = SetReducer.getExpandedHistorySet(state.sets, state.history.expandedSetID);
-		var reps = set.reps;
+		var repNum = 0;
+		var vms = set.reps.map((rep) => {
+			repNum++;
+			let data = rep.data;
+
+			var duration = RepDataMap.durationOfLift(data)
+			if (duration !== null) {
+				duration = (duration / 1000000.0).toFixed(2);
+			} else {
+				duration = "obv2 only";
+			}
+
+			return {
+				key: "rep"+repNum,
+				removed: rep.removed,
+				values: [
+					repNum,
+					RepDataMap.averageVelocity(data),
+					RepDataMap.peakVelocity(data),
+					RepDataMap.peakVelocityLocation(data),
+					RepDataMap.rangeOfMotion(data),
+					duration,
+				]
+			};
+		});
 	}
 
 	return {
 		visible: state.history.expandedSetID !== null,
-		reps: reps
+		rows: vms
 	}
 };
 

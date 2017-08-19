53f48c622e8754d18c11850b9bdd0bcd4a3d2ea2
JonnyLinja <jonny.linja@gmail.com>
2017-08-19 05:10:12 -0400

    Remove settingsactioncreator as saving the value only happens in settings now

--- app/redux/shared_actions/SettingsActionCreators.js
+++
@@ -1,8 +0,0 @@
-// These exist as a shared action creator because saving values can be done from all over the app
-
-import { SAVE_END_SET_TIMER } from 'app/ActionTypes';
-
-export const saveEndSetTimer = (duration = 30) => ({
-    type: SAVE_END_SET_TIMER,
-    endSetTimerDuration: duration
-});

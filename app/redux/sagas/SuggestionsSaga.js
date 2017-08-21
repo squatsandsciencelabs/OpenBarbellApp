2cc8e965a57f3b52c8d9d5c33b7e79849b0a3c55
JonnyLinja <jonny.linja@gmail.com>
2017-08-21 00:08:31 -0400

    Suggestions now run on startup

--- app/redux/sagas/SuggestionsSaga.js
+++ app/redux/sagas/SuggestionsSaga.js
@@ -1,14 +1,15 @@
 import { take, put } from 'redux-saga/effects';
 import {
     LOGIN_SUCCESS,
-    LOGOUT
+    LOGOUT,
+    STORE_INITIALIZED
 } from 'app/ActionTypes';
 
 import * as SuggestionsActionCreators from 'app/redux/shared_actions/SuggestionsActionCreators';
 
 const SuggestionsSaga = function * SuggestionsSaga() {
     while (true) {
-        yield take([LOGIN_SUCCESS, LOGOUT]);
+        yield take([LOGIN_SUCCESS, LOGOUT, STORE_INITIALIZED]);
         yield put(SuggestionsActionCreators.updateSuggestions());
     }
 };

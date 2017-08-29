402bfc894ce94257d8c79d13e676f3ee3330d7d0
Malek <mmalek1421@gmail.com>
2017-08-29 13:20:44 -0400

    fixed bug where OB status would not change

--- app/features/workout/bottom_bar/device_status/ConnectedDeviceStatus.js
+++ app/features/workout/bottom_bar/device_status/ConnectedDeviceStatus.js
@@ -14,7 +14,7 @@ class ConnectedDeviceStatus extends Component {
         if (this.props.deviceStatus === 'CONNECTED') {
             return <View style={styles.statusBar}><Image style={styles.imageStyle} source={require('app/appearance/images/icon_connected.png')} /><Text style={styles.textStyle}> {this.props.deviceName} </Text></View>
         } else {
-            return <View style={styles.statusBar}><Image style={styles.imageStyle} source={require('app/appearance/images/icon_disconnected.png')} /><Text style={styles.textStyle}> Not Connected</Text></View>
+            return <View style={styles.statusBar}><Image style={styles.imageStyle} source={require('app/appearance/images/icon_disconnected.png')} /><Text style={styles.textStyle}> NOT CONNECTED</Text></View>
         }
     }
 }
@@ -25,10 +25,12 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'flex-start',
         alignItems: 'center',
-        paddingLeft: 5
+        paddingLeft: 10
     },
     textStyle: {
-        color: 'white'
+        color: 'white',
+        paddingLeft: 5,
+        fontSize: 11
     },
     imageStyle: {
         tintColor: 'white'

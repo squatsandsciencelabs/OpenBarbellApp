f2a6501eaf3be2967813828039911426ac16c2fd
Malek <mmalek1421@gmail.com>
2017-08-29 13:20:44 -0400

    fixed bug where OB status would not change

--- app/features/workout/bottom_bar/WorkoutBottomBar.js
+++ app/features/workout/bottom_bar/WorkoutBottomBar.js
@@ -7,7 +7,7 @@ import {
     StyleSheet,
     TouchableHighlight
 } from 'react-native';
-import ConnectedDeviceStatus from './device_status/ConnectedDeviceStatus';
+import ConnectedDeviceStatusScreen from './device_status/ConnectedDeviceStatusScreen';
 
 class WorkoutBottomBar extends Component {
 
@@ -26,9 +26,9 @@ class WorkoutBottomBar extends Component {
         return (
             <View style={styles.bar}>
                 { this._renderDevice() }
-                <ConnectedDeviceStatus />
+                <ConnectedDeviceStatusScreen />
                 <TouchableHighlight style={{justifyContent: 'center'}} onPress={ () => this._onPressEndWorkout() } activeOpacity={1} >
-                    <Text style={styles.buttonText}>End Workout</Text>
+                    <Text style={styles.buttonText}>FINISH WORKOUT</Text>
                 </TouchableHighlight>
             </View>
         );

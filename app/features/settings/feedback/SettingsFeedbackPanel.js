46fde59cd681007af7c7776ae091b3018a8ac590
Malek <mmalek1421@gmail.com>
2017-08-31 22:54:29 -0400

    added feedback to settings

---
+++ app/features/settings/feedback/SettingsFeedbackPanel.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {
+    Text,
+    View,
+    TouchableOpacity,
+    Linking
+} from 'react-native';
+import { SETTINGS_PANEL_STYLES } from 'app/appearance/styles/GlobalStyles';
+
+class SettingsFeedbackPanel extends Component {
+    render() {
+        return (
+            <View style={ [SETTINGS_PANEL_STYLES.panel, { flex: 1, flexDirection:'column', justifyContent:'space-between' }] }>
+                <Text style={ SETTINGS_PANEL_STYLES.headerText }>
+                    Feedback {"\n"}
+                </Text>    
+                <TouchableOpacity onPress={() => Linking.openURL('mailto:contact@squatsandscience.com?subject=A%20really%20nice%20comment%20from%20an%20app%20user&body=')}>
+                    <Text style={SETTINGS_PANEL_STYLES.tappableText}>
+                        contact@squatsandscience.com
+                    </Text>
+                </TouchableOpacity>         
+            </View>
+        );        
+    }
+}
+
+export default SettingsFeedbackPanel;

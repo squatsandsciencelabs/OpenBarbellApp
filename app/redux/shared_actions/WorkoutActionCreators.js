bdf5a36c5ef77a21fab96d510e4b9afa444fa528
Malek <mmalek1421@gmail.com>
2017-09-06 22:00:43 -0400

    end workout issues warning if logged out

--- app/redux/shared_actions/WorkoutActionCreators.js
+++ app/redux/shared_actions/WorkoutActionCreators.js
@@ -1,14 +1,26 @@
 // This exists in shared actions because the timer can also end the workout
-
+import { Alert } from 'react-native';
 import * as SetsSelectors from 'app/redux/selectors/SetsSelectors';
+import * as AuthSelectors from 'app/redux/selectors/AuthSelectors';
 import { END_WORKOUT } from 'app/ActionTypes';
 
 export const endWorkout = () => (dispatch, getState) => {
     var state = getState();
     var getIsWorkingSetEmpty = SetsSelectors.getIsWorkingSetEmpty(state)
+    var isLoggedIn = AuthSelectors.getIsLoggedIn(state);
 
-    if (getIsWorkingSetEmpty) {
+    if (getIsWorkingSetEmpty && isLoggedIn) {
         dispatch({ type: END_WORKOUT });
+    } else if(!isLoggedIn) {
+        Alert.alert(
+            'Heads up!',
+            "You are not logged in, the data from this workout will be lost,\nPlease sign in under settings to save your data",
+            [
+              {text: 'Continue', onPress: () => dispatch({ type: END_WORKOUT })},
+              {text: 'Quit', style: 'cancel'},,
+            ],
+            { cancelable: false }
+          )                    
     }
 };
 

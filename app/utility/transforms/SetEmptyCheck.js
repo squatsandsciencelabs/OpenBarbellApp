e069baa8e685bb3e91ee38ffe9d3f2ed76a9ea6e
JonnyLinja <jonny.linja@gmail.com>
2017-09-19 18:05:20 -0400

    ending a workout will not add working set if working set is blank

--- app/utility/transforms/SetEmptyCheck.js
+++ app/utility/transforms/SetEmptyCheck.js
@@ -1,13 +1,23 @@
+// no data and no active reps
 export const isEmpty = (set) => {
     return hasEmptyData(set) && hasEmptyReps(set);
+};
+
+// no data and no reps at all
+export const isBlank = (set) => {
+    return hasEmptyData(set) && hasNoReps(set);
 };
 
 export const hasEmptyData = (set) => {
     return !set.exercise && (!set.weight || set.weight === '') && (!set.rpe || set.rpe === '') && (!set.tags || set.tags === undefined || set.tags.length === 0) && !set.videoFileURL;
 };
 
+export const hasNoReps = (set) => {
+    return set.reps !== null && set.reps !== undefined && set.reps.length === 0;
+};
+
 export const hasEmptyReps = (set) => {
-    if (set.reps !== null && set.reps !== undefined && set.reps.length === 0) {
+    if (hasNoReps(set)) {
         return true;
     }
 

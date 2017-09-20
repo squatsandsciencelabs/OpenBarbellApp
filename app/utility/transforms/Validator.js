88b0f9fbb86164734a1dba3db6f13f9f20b5639a
JonnyLinja <jonny.linja@gmail.com>
2017-09-20 01:19:21 -0400

    Properly set lastrefreshdate on login. Refactored validator

--- app/utility/transforms/Validator.js
+++ app/utility/transforms/Validator.js
@@ -2,11 +2,19 @@ import OpenBarbellConfig from 'app/configs/OpenBarbellConfig.json';
 import * as DateUtils from 'app/utility/transforms/DateUtils';
 
 export default class Validator {
+
     constructor(accessToken, lastRefreshDate) {
         this.accessToken = accessToken
-        this.lastRefreshSeconds = DateUtils.getDate(lastRefreshDate).getTime()
-        this.todaysDateSeconds = (new Date()).getTime()
-        this.secondsBetween = this.todaysDateSeconds - this.lastRefreshSeconds
-        this.isValid = this.secondsBetween < OpenBarbellConfig.apiWaitTimer;
+        if (!lastRefreshDate) {
+            // no refresh date, invalid
+            this.isValid = false;
+        } else {
+            // calculate isvalid
+            let lastRefreshMS = DateUtils.getDate(lastRefreshDate).getTime();
+            let todaysDateMS = (new Date()).getTime();
+            let msBetween = todaysDateMS - lastRefreshMS;
+            this.isValid = msBetween < OpenBarbellConfig.apiWaitTimer;
+        }
     }
+
 };

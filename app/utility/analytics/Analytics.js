a95fb6a2d71666b019821f87d25495d9127cb52a
Malek <mmalek1421@gmail.com>
2017-09-29 21:39:50 -0400

    added analytics wrapper

---
+++ app/utility/analytics/Analytics.js
@@ -0,0 +1,19 @@
+import firebase from 'app/configs/firebase';
+
+// Screens
+
+export const setCurrentScreen = (screen) => {
+    firebase.analytics().setCurrentScreen(screen);
+};
+
+// User Properties
+
+export const setUserProp = (name, value) => {
+    firebase.analytics().setUserProperty(name, value);
+};
+
+// Log Events
+// params must be an object
+export const logEvent = (event, params) => {
+    firebase.analytics().logEvent(event, name)
+};

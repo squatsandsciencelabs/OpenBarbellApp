82cc7da6f5d812369ad63a126304e43cc7c16fed
Malek <mmalek1421@gmail.com>
2017-09-30 20:45:13 -0400

    added firebase settings

--- app/features/history/video/HistoryVideoPlayerActions.js
+++ app/features/history/video/HistoryVideoPlayerActions.js
@@ -2,12 +2,17 @@ import {
     DELETE_HISTORY_VIDEO,
     DISMISS_HISTORY_VIDEO_PLAYER
 } from 'app/ActionTypes';
+import * as Analytics from 'app/utility/analytics/Analytics';
 
 export const deleteVideo = (setID) => ({
     type: DELETE_HISTORY_VIDEO,
     setID: setID
 });
 
-export const closeModal = () => ({
-    type: DISMISS_HISTORY_VIDEO_PLAYER
-});
+export const closeModal = () => {
+    Analytics.setCurrentScreen('history');
+    
+    return {
+        type: DISMISS_HISTORY_VIDEO_PLAYER
+    }
+};

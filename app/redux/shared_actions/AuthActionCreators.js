f962cdf4ec93fa7e52cf75952f6d7cb1bda72c5f
Malek <mmalek1421@gmail.com>
2017-10-04 13:48:00 -0400

    added user props

--- app/redux/shared_actions/AuthActionCreators.js
+++ app/redux/shared_actions/AuthActionCreators.js
@@ -5,18 +5,25 @@ import {
     SAVE_TOKENS,
     TOKENS_READY
 } from 'app/ActionTypes';
+import * as Analytics from 'app/utility/analytics/Analytics';
 
-export const loginSucceeded = (accessToken, refreshToken, email, date = new Date(), revision = null, sets = null) => ({
-    type: LOGIN_SUCCESS,
-    accessToken: accessToken,
-    refreshToken: refreshToken,
-    email: email,
-    syncDate: date,
-    revision: revision,
-    sets: sets
-});
+export const loginSucceeded = (accessToken, refreshToken, email, date = new Date(), revision = null, sets = null) => {
+    Analytics.setUserProp('email', email);
+
+    return {
+        type: LOGIN_SUCCESS,
+        accessToken: accessToken,
+        refreshToken: refreshToken,
+        email: email,
+        syncDate: date,
+        revision: revision,
+        sets: sets
+    }
+};
 
 export const logout = (showMessage = false) => {
+    Analytics.setUserProp('email', '');
+
     if (showMessage) {
         Alert.alert("Important", "As it's been awhile since you've signed on, you've been logged out! Please login again.");
     }

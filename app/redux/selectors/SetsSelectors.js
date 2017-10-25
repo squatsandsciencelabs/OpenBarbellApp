e8becee318d04a3e3346cc24710b0839ed9a3e87
JonnyLinja <jonny.linja@gmail.com>
2017-10-25 02:45:16 -0400

    Renamed some setselector functions and reorganized setsselectors

--- app/redux/selectors/SetsSelectors.js
+++ app/redux/selectors/SetsSelectors.js
@@ -7,16 +7,12 @@ import * as DurationCalculator from 'app/utility/transforms/DurationCalculator';
 
 const stateRoot = (state) => state.sets;
 
-// Get revision
-
-export const getRevision = (state) => stateRoot(state).revision;
-
-// Get last rep time
+// Workout
 
 // TODO: fix bug here because I'm no longer using set's end and start time
 // apparently I used it for more than just rest timer fml
 
-export const lastRepTime = (state) => {
+export const lastWorkoutRepTime = (state) => {
     // check if there's workout data
     var workoutData = stateRoot(state).workoutData;
     if (workoutData.length <= 0) {
@@ -45,8 +41,6 @@ export const lastRepTime = (state) => {
     return null;
 }
 
-// Get Workout Sets
-
 export const getWorkoutSets = (state) => {
     return stateRoot(state).workoutData;
 };
@@ -54,8 +48,6 @@ export const getWorkoutSets = (state) => {
 export const getNumWorkoutSets = (state) => {
     return getWorkoutSets(state).length;
 };
-
-// Get Is Workout Empty
 
 export const getIsWorkoutEmpty = (state) => {
     const workoutData = stateRoot(state).workoutData;
@@ -71,130 +63,8 @@ export const getIsWorkoutEmpty = (state) => {
     return true;
 };
 
-// Get Expanded Workout Set
-
 export const getExpandedWorkoutSet = (state, setID) => {
     return state.workoutData.find( set => set.setID == setID );
-};
-
-// Get previous set
-
-export const getPreviousSetHasEmptyReps = (state) => {
-    const workoutData = stateRoot(state).workoutData;
-    const prevSet = workoutData[workoutData.length - 2];
-    
-    if (prevSet) {
-        return SetEmptyCheck.hasEmptyReps(prevSet);
-    } else {
-        return false;
-    }
-}
-
-export const getIsPreviousSetFilled = (state) => {
-    const workoutData = stateRoot(state).workoutData;    
-
-    const prevSet = workoutData[workoutData.length - 2];
-
-    if (prevSet) {
-        if(SetEmptyCheck.isEmpty(prevSet)) {
-            var is_previous_set_filled = 0;
-        } else {
-            var is_previous_set_filled = 1;
-        }
-    } else {
-        var is_previous_set_filled = -1;
-    }  
-    
-    return is_previous_set_filled;
-}
-
-// Dictionary to Array
-
-const dictToArray = (dictionary) => {
-    var array = [];
-    for (var property in dictionary) {
-        if (dictionary.hasOwnProperty(property)) {
-            array.push(dictionary[property]);
-        }
-    }
-    return array;
-};
-
-// Get History Sets
-
-export const getHistorySetsChronological = (state) => {
-    let sets = stateRoot(state);
-    var array = dictToArray(sets.historyData);
-    array.sort((set1, set2) => new Date(set1.startTime) - new Date(set2.startTime));
-    return array;
-};
-
-export const getNumHistorySets = (state) => {
-    let sets = stateRoot(state);
-    var array = dictToArray(sets.historyData);
-    return array.length;
-};
-
-export const getNumHistoryReps = (state) => {
-    let sets = getHistorySetsChronological(state);
-
-    var num_reps = 0;
-    
-    sets.forEach((set) => {
-        if (set.reps) {
-            num_reps += set.reps.length;
-        }
-    });
-    
-    return num_reps;    
-}
-
-export const getHistoryWorkoutIDs = (state) => {
-    let sets = getHistorySetsChronological(state);
-    
-    let workoutIDs = [sets[0].workoutID];
-
-    for (var i = 1; i < sets.length; i++) {
-        if (sets[i].workoutID !== sets[i - 1].workoutID) {
-            workoutIDs.push(sets[i].workoutID);
-        }
-    }
-
-    return workoutIDs;
-}
-
-export const getNumHistoryWorkouts = (state) => {
-    return getHistoryWorkoutIDs(state).length;
-};
-
-// Get Expanded History Set
-
-export const getExpandedHistorySet = (state, setID) => {
-    var dictionary = stateRoot(state).historyData;
-    for (var property in dictionary) {
-        if (dictionary.hasOwnProperty(property)) {
-            let set = dictionary[property];
-            if (set.setID === setID) {
-                return set;
-            }
-        }
-    }
-    return null;
-};
-
-// Get Sets To Upload
-export const getSetsToUpload = (state) => {
-    const root = stateRoot(state);
-    return root.setIDsToUpload.map( setID => root.historyData[setID] );
-};
-
-// Get is uploading
-export const getIsUploading = (state) => stateRoot(state).setIDsBeingUploaded.length > 0;
-
-// Has Changes To Sync
-export const hasChangesToSync = (state) => {
-    const root = stateRoot(state);
-    return (root.setIDsToUpload.length > 0 || root.setIDsBeingUploaded.length > 0);
 };
 
 export const getNumWorkoutReps = (state) => {
@@ -257,9 +127,126 @@ export const getIsWorkingSet = (state, setID) => {
     return setID === currentSet.setID;
 };
 
+export const getPreviousWorkoutSetHasEmptyReps = (state) => {
+    const workoutData = stateRoot(state).workoutData;
+    const prevSet = workoutData[workoutData.length - 2];
+    
+    if (prevSet) {
+        return SetEmptyCheck.hasEmptyReps(prevSet);
+    } else {
+        return false;
+    }
+}
+
+export const getIsPreviousWorkoutSetFilled = (state) => {
+    const workoutData = stateRoot(state).workoutData;    
+
+    const prevSet = workoutData[workoutData.length - 2];
+
+    if (prevSet) {
+        if(SetEmptyCheck.isEmpty(prevSet)) {
+            var is_previous_set_filled = 0;
+        } else {
+            var is_previous_set_filled = 1;
+        }
+    } else {
+        var is_previous_set_filled = -1;
+    }  
+    
+    return is_previous_set_filled;
+}
+
+// Dictionary to Array
+
+const dictToArray = (dictionary) => {
+    var array = [];
+    for (var property in dictionary) {
+        if (dictionary.hasOwnProperty(property)) {
+            array.push(dictionary[property]);
+        }
+    }
+    return array;
+};
+
+// History
+
+export const getHistorySetsChronological = (state) => {
+    let sets = stateRoot(state);
+    var array = dictToArray(sets.historyData);
+    array.sort((set1, set2) => new Date(set1.startTime) - new Date(set2.startTime));
+    return array;
+};
+
+export const getNumHistorySets = (state) => {
+    let sets = stateRoot(state);
+    var array = dictToArray(sets.historyData);
+    return array.length;
+};
+
+export const getNumHistoryReps = (state) => {
+    let sets = getHistorySetsChronological(state);
+
+    var num_reps = 0;
+    
+    sets.forEach((set) => {
+        if (set.reps) {
+            num_reps += set.reps.length;
+        }
+    });
+    
+    return num_reps;    
+}
+
+export const getHistoryWorkoutIDs = (state) => {
+    let sets = getHistorySetsChronological(state);
+    
+    let workoutIDs = [sets[0].workoutID];
+
+    for (var i = 1; i < sets.length; i++) {
+        if (sets[i].workoutID !== sets[i - 1].workoutID) {
+            workoutIDs.push(sets[i].workoutID);
+        }
+    }
+
+    return workoutIDs;
+}
+
+export const getNumHistoryWorkouts = (state) => {
+    return getHistoryWorkoutIDs(state).length;
+};
+
+export const getExpandedHistorySet = (state, setID) => {
+    var dictionary = stateRoot(state).historyData;
+    for (var property in dictionary) {
+        if (dictionary.hasOwnProperty(property)) {
+            let set = dictionary[property];
+            if (set.setID === setID) {
+                return set;
+            }
+        }
+    }
+    return null;
+};
+
 export const lastWorkoutTime = (state) => {
     let sets = getHistorySetsChronological(state);
     let startTime = Date.parse(SetTimeCalculator.startTime(sets[sets.length - 1]));
 
     return Date.parse(new Date()) - startTime;
 };
+
+// Syncing
+
+export const getSetsToUpload = (state) => {
+    const root = stateRoot(state);
+    return root.setIDsToUpload.map( setID => root.historyData[setID] );
+};
+
+export const getIsUploading = (state) => stateRoot(state).setIDsBeingUploaded.length > 0;
+
+export const hasChangesToSync = (state) => {
+    const root = stateRoot(state);
+    return (root.setIDsToUpload.length > 0 || root.setIDsBeingUploaded.length > 0);
+};
+
+export const getRevision = (state) => stateRoot(state).revision;

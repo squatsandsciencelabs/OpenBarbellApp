0b4e6a56542ac44da2ffa9e5e43d75c42a0d9145
JonnyLinja <jonny.linja@gmail.com>
2017-10-26 17:49:37 -0400

    Tweaked Firebase setup. Made tests work properly around Firebase

--- app/configs/Firebase.js
+++ app/configs/Firebase.js
@@ -1,9 +1,3 @@
-import RNFirebase from 'react-native-firebase';
+import firebase from 'react-native-firebase';
 
-const configurationOptions = {
-  debug: true
-};
-
-const firebase = RNFirebase.initializeApp(configurationOptions);
-
-export default firebase;
+export default firebase.app();

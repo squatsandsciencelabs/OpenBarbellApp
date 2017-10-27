a85b7ed185cc6b236cb3d055fa3f03c294fb593c
JonnyLinja <jonny.linja@gmail.com>
2017-10-27 01:08:00 -0400

    Added tests for hasNoReps and hasEmptyReps. Tweaked code for some edge cases

--- __tests__/SetEmptyCheck.test.js
+++ __tests__/SetEmptyCheck.test.js
@@ -1,6 +1,7 @@
 import * as sut from 'app/utility/transforms/SetEmptyCheck';
 
 describe('hasEmptyData', () => {
+    // note: not testing for every undefined / null case as too many possibilities
     // note: not testing for combinations as too many possibilities
     test('true when null', () => {
         let set = {
@@ -99,4 +100,110 @@ describe('hasEmptyData', () => {
 
         expect(result).toBeFalsy();
     });
+});
+
+describe('hasNoReps', () => {
+    test('0 reps true', () => {
+        let set = {
+            reps: []
+        };
+
+        let result = sut.hasNoReps(set);
+
+        expect(result).toBeTruthy();
+    });
+
+    test('undefined true', () => {
+        let set = {
+        };
+
+        let result = sut.hasNoReps(set);
+
+        expect(result).toBeTruthy();
+    });
+
+    test('null true', () => {
+        let set = {
+            reps: null
+        };
+
+        let result = sut.hasNoReps(set);
+
+        expect(result).toBeTruthy();
+    });
+
+    test('false', () => {
+        let set = {
+            reps: [{}]
+        };
+
+        let result = sut.hasNoReps(set);
+
+        expect(result).toBeFalsy();
+    });
+});
+
+describe('hasEmptyReps', () => {
+    test('0 reps true', () => {
+        let set = {
+            reps: []
+        };
+
+        let result = sut.hasEmptyReps(set);
+
+        expect(result).toBeTruthy();
+    });
+
+    test('undefined true', () => {
+        let set = {
+        };
+
+        let result = sut.hasEmptyReps(set);
+
+        expect(result).toBeTruthy();
+    });
+
+    test('null true', () => {
+        let set = {
+            reps: null
+        };
+
+        let result = sut.hasEmptyReps(set);
+
+        expect(result).toBeTruthy();
+    });
+
+    test('true when only undefined removes', () => {
+        let set = {
+            reps: [{}, {}]
+        };
+
+        let result = sut.hasEmptyReps(set);
+
+        expect(result).toBeTruthy();
+    });
+
+    test('true when everything is removed', () => {
+        let set = {
+            reps: [{removed: true}, {removed: true}]
+        };
+
+        let result = sut.hasEmptyReps(set);
+
+        expect(result).toBeTruthy();
+    });
+
+    test('false when at least one not removed', () => {
+        let set = {
+            reps: [{removed: true}, {}, {removed: false}]
+        };
+
+        let result = sut.hasEmptyReps(set);
+
+        expect(result).toBeFalsy();
+    });
+});
+
+describe('numFieldsEntered', () => {
+
 });

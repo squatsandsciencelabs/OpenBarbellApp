50fae38ee56caa5f872dae527d8977167d3ae8ab
JonnyLinja <jonny.linja@gmail.com>
2017-10-29 02:44:31 -0400

    Tapped on device in workout now shows Settings

--- app/features/application/ApplicationActions.js
+++ app/features/application/ApplicationActions.js
@@ -1,27 +1,5 @@
-import { Keyboard } from 'react-native';
-import * as Analytics from 'app/utility/Analytics';
-import { HISTORY_VIEWED } from 'app/ActionTypes';
+import * as AppStateActionCreators from 'app/redux/shared_actions/AppStateActionCreators';
 
-import {
-    CHANGE_TAB
-} from 'app/ActionTypes';
-
-export const changeTab = (tabIndex) => (dispatch) => {
-    switch(tabIndex) {
-        case 0:
-            Analytics.setCurrentScreen('workout');
-            break;
-        case 1:
-            Analytics.setCurrentScreen('history');
-            dispatch({ type: HISTORY_VIEWED });
-            break;
-        case 2: 
-            Analytics.setCurrentScreen('settings');
-            break;
-    }
-    Keyboard.dismiss();    
-    dispatch({
-        type: CHANGE_TAB,
-        tabIndex: tabIndex
-    });
+export const changeTab = (tabIndex) => {
+    return AppStateActionCreators.changeTab(tabIndex);
 };

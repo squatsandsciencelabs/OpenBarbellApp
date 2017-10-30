07dba379c35b3bc6a4d0d934e3563e2d409f6913
JonnyLinja <jonny.linja@gmail.com>
2017-10-30 03:22:19 -0400

    logout_cancelled analytics

--- app/features/settings/account/SettingsAccountScreen.js
+++ app/features/settings/account/SettingsAccountScreen.js
@@ -24,6 +24,7 @@ const mapDispatchToProps = (dispatch) => {
         showRemoved: Actions.showRemovedData,
         hideRemoved: Actions.hideRemovedData,
         exportCSV: Actions.exportCSV,
+        cancelSignOut: Actions.cancelSignOut,
     }, dispatch);
 };
 

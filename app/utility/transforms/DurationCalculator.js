b1631859ed258610c683d702647d74eeaef39af8
JonnyLinja <jonny.linja@gmail.com>
2017-11-01 17:42:47 -0400

    Added comment

--- app/utility/transforms/DurationCalculator.js
+++ app/utility/transforms/DurationCalculator.js
@@ -1,5 +1,7 @@
 // duration calculator
 
+// TODO: do we want this to be negative ever? Or just 0 as minimum?
+// ABS is dangerous
 export const getDurationBetween = (startDate, endDate) => {
     return Math.abs(Date.parse(endDate) - Date.parse(startDate));
 };

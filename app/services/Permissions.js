30206daa40fee986c5bca7cb25cce9feb38f8c5a
JonnyLinja <jonny.linja@gmail.com>
2017-11-02 02:23:46 -0400

    Switching to PermissionsAndroid to solve multiple request issues

--- app/services/Permissions.js
+++ app/services/Permissions.js
@@ -1,18 +1,23 @@
 // TODO: Move Android permissions that are currently done natively in Java to here
 // Exception is bluetooth as the library doesn't appear to handle Bluetooth on Android
 
+// TODO: Just use the library when they fix Android permissions issues
 // TODO: Permissions alert wrapper so don't lose the first iOS permission request
 
 import Permissions from 'react-native-permissions';
-import { Platform } from 'react-native';
+import { Platform, PermissionsAndroid } from 'react-native';
 
 export default function() {
-    Permissions.request('photo')
-    .then((photoReponse) => { return Permissions.request('camera') })
-    .then((cameraResponse) => { return Permissions.request('microphone') })
-    .then((microphoneResponse) => {
-        if (Platform.OS !== 'ios') {        
-            Permissions.request('storage');
-        }
-    });
+    if (Platform.OS !== 'ios') {
+        PermissionsAndroid.requestMultiple([
+            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+            PermissionsAndroid.PERMISSIONS.CAMERA,
+            PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+            PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE
+        ]);
+    } else {
+        Permissions.request('photo');
+        Permissions.request('camera');
+        Permissions.request('microphone');
+    }
 };

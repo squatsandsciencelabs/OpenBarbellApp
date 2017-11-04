cd34ae394cf867d3d7ea602f7dc76f3ff45cdd9b
JonnyLinja <jonny.linja@gmail.com>
2017-11-03 23:48:09 -0400

    Basic actions for collapsed

--- app/features/history/HistoryActions.js
+++ app/features/history/HistoryActions.js
@@ -1,9 +1,25 @@
 import {
     PRESENT_HISTORY_EXPANDED,
     LOADING_HISTORY,
-    SAVE_HISTORY_REP
+    SAVE_HISTORY_REP,
+    COLLAPSE_HISTORY_SET,
+    EXPAND_HISTORY_SET,
 } from 'app/ActionTypes';
 import * as Analytics from 'app/services/Analytics';
+
+export const collapseSet = (setID) => (dispatch, getState) => {
+    dispatch({
+        type: COLLAPSE_HISTORY_SET,
+        setID: setID,
+    });
+};
+
+export const expandSet = (setID) => (dispatch, getState) => {
+    dispatch({
+        type: EXPAND_HISTORY_SET,
+        setID: setID,
+    });
+};
 
 export const presentExpanded = (setID) => ({
     type: PRESENT_HISTORY_EXPANDED,

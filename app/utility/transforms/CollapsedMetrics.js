e2813078fe87845ccea6e6efaa9a863ecddd8c43
Malek <mmalek1421@gmail.com>
2017-11-09 18:11:13 -0500

    tests for collapsed utility

--- app/utility/transforms/CollapsedMetrics.js
+++ app/utility/transforms/CollapsedMetrics.js
@@ -4,54 +4,78 @@ export const getAvgVelocities = (set) => {
     let velocities = [];
     
     for (let i = 0; i < set.reps.length; i++) {
-        let rep = matchedSet.reps[i];
+        let rep = set.reps[i];
                 
-        if (rep.isValid == true) {
+        if (rep.isValid === true && rep.removed === false) {
             let repData = rep.data;
         
-            velocities.push(RepDataMap.averageVelocity(repData));
+            velocities.push(Number(RepDataMap.averageVelocity(repData)));
         }            
     };
 
-    return velocities.map(vel => Number(vel));;
+    return velocities;
 };
 
 export const getAvgofAvgVelocities = (set) => {
-    let avgVs = getAvgVelocites(set);
+    let avgVs = getAvgVelocities(set);
 
-    let sum = avgVs.reduce((previous, current) => current += previous);
-    return sum / avgVs.length;    
+    if (avgVs.length > 0) {
+        let sum = avgVs.reduce((previous, current) => current += previous);
+        return (sum / avgVs.length).toFixed(2);
+    } else {
+        return null;
+    }  
 };
 
 export const getAbsLossVelocity = (set) => {
     let avgVs = getAvgVelocities(set);
 
-    let maxV = Math.max(...avgVs);
-    let minV = Math.min(...avgVs);
-
-    return maxV - minV;
+    if (avgVs.length > 0) {
+        let maxV = Math.max(...avgVs);
+        let minV = Math.min(...avgVs);
+        
+        return maxV - minV;
+    } else {
+        return null;
+    }
 };
 
 export const getFirstRepAvgVelocity = (set) => {
     let avgVs = getAvgVelocities(set);
     
-    return avgVs[0];
+    if (avgVs.length > 0) {
+        return avgVs[0];
+    } else {
+        return null;
+    }
 };
 
 export const getLastRepAvgVelocity = (set) => {
     let avgVs = getAvgVelocities(set);
     
-    return avgVs[avgVs.length - 1];
+    if (avgVs.length > 0) {
+        return avgVs[avgVs.length - 1];
+    } else {
+        return null;
+    }
 };
 
 export const getMinAvgVelocity = (set) => {
     let avgVs = getAvgVelocities(set);
 
-    return Math.min(...avgVs);
+    if (avgVs.length > 0) {
+        return Math.min(...avgVs);
+    } else {
+        return null;
+    }
 };
 
 export const getMaxAvgVelocity = (set) => {
     let avgVs = getAvgVelocities(set);
     
-    return Math.max(...avgVs);    
+    if (avgVs.length > 0) {
+        return Math.max(...avgVs);    
+    } else {
+        return null;
+    }
 };

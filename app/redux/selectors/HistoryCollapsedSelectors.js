a4a03c8286eb8d075b5d8c93869c1a0cf0449def
JonnyLinja <jonny.linja@gmail.com>
2017-11-10 03:19:01 -0500

    History collapsed and fixed video

--- app/redux/selectors/HistoryCollapsedSelectors.js
+++ app/redux/selectors/HistoryCollapsedSelectors.js
@@ -1,3 +1,7 @@
 const stateRoot = (state) => state.historyCollapsed;
 
-export const getIsCollapsed = (state, setID) => stateRoot(state)[setID] === true;
+export const getIsCollapsed = (state, setID) => {
+    return stateRoot(state)[setID] !== false;
+};
+
+export const getCollapsedModel = (state) => stateRoot(state);

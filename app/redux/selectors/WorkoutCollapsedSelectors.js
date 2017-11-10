1732dc9b805f683adc87b5d168f2f033a3246676
JonnyLinja <jonny.linja@gmail.com>
2017-11-10 00:54:48 -0500

    Default closed

--- app/redux/selectors/WorkoutCollapsedSelectors.js
+++ app/redux/selectors/WorkoutCollapsedSelectors.js
@@ -1,7 +1,5 @@
 const stateRoot = (state) => state.workoutCollapsed;
 
 export const getIsCollapsed = (state, setID) => {
-    let returnval = stateRoot(state)[setID] === true;
-    console.tron.log(stateRoot(state) + " " + setID + " " + stateRoot(state)[setID] + " asdf " + returnval);
-    return stateRoot(state)[setID] === true;
-}
+    return stateRoot(state)[setID] !== false;
+};

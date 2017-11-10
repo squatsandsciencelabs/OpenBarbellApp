aa3b0cff870713c2bbdb6d2a827c503204f4f6a0
JonnyLinja <jonny.linja@gmail.com>
2017-11-09 21:57:45 -0500

    Connected analysis and summary placeholder views

--- app/shared_features/set_card/expanded/SetTitleRowExpanded.js
+++ app/shared_features/set_card/expanded/SetTitleRowExpanded.js
@@ -10,54 +10,41 @@ import {
 import Icon from 'react-native-vector-icons/Entypo';
 
 class SetTitleRowExpanded extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            setNumber: this.props.setNumber,
-            exercise: this.props.exercise,
-            removed: this.props.removed,
-        };
-    }
-
-    componentWillReceiveProps(nextProps) {
-        this.setState({
-            setNumber: nextProps.setNumber,
-            exercise: nextProps.exercise,
-            removed: nextProps.removed,
-        });
-    }
 
     _renderExercise() {
-        if (this.state.exercise === null || this.state.exercise === '') {
+        if (this.props.exercise === null || this.props.exercise === '') {
             return (<Text style={[styles.fieldText, styles.placeholderText]}>Exercise</Text>);
         }
-        return (<Text style={styles.fieldText}>{this.state.exercise}</Text>);
+        return (<Text style={styles.fieldText}>{this.props.exercise}</Text>);
     }
 
     _renderSetNumber() {
-        if (this.state.removed) {
+        if (this.props.removed) {
             return null;
         }
-        if (this.state.setNumber === null || this.state.setNumber === undefined) {
+        if (this.props.setNumber === null || this.props.setNumber === undefined) {
             return '#1';
         }
-        return '#' + this.state.setNumber;
+        return '#' + this.props.setNumber;
     }
 
     _renderChevron() {
-        return (
-            <View style={{backgroundColor: 'white', marginLeft: 50, marginTop: 5, marginBottom: 10}}>
-                <Icon name="chevron-with-circle-down" size={20} color='rgba(170, 170, 170, 1)' />
-            </View>
-        );
+        if (this.props.isCollapsable) {
+            return (
+                <View style={{backgroundColor: 'white', marginLeft: 50, marginTop: 5, marginBottom: 10}}>
+                    <Icon name="chevron-with-circle-down" size={20} color='rgba(170, 170, 170, 1)' />
+                </View>
+            );
+        } else {
+            return null;
+        }
     }
 
     render() {
         return (
             <View style={[styles.container, styles.border]}>
                 <View style={[styles.field, {flex: 1}]}>
-                    <TouchableHighlight onPress={() => this.props.tapExercise(this.props.setID, this.state.exercise, this.props.bias)} underlayColor='#e0e0e0'>
+                    <TouchableHighlight onPress={() => this.props.tapExercise(this.props.setID, this.props.exercise, this.props.bias)} underlayColor='#e0e0e0'>
                         {this._renderExercise()}
                     </TouchableHighlight>
                     <View style={styles.fieldDetails} pointerEvents='none'>

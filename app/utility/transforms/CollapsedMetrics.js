59e4df9f7a3972a72c4f61c8216064814309adc5
JonnyLinja <jonny.linja@gmail.com>
2017-11-15 23:30:34 -0500

    Refactored collapsedmetrics

--- app/utility/transforms/CollapsedMetrics.js
+++ app/utility/transforms/CollapsedMetrics.js
@@ -2,132 +2,90 @@ import * as RepDataMap from 'app/utility/transforms/RepDataMap';
 
 // unique metrics
 
-export const getAvgVelocities = (set) => {
-    const velocities = [];
+const getMetrics = (set, metricFunction) => {
+    const metrics = [];
     
     set.reps.forEach((rep) => {                
         if (rep.isValid === true && rep.removed === false) {
             let repData = rep.data;
         
-            velocities.push(Number(RepDataMap.averageVelocity(repData)));
-        }            
+            metrics.push(Number(metricFunction(repData)));
+        }
     });
 
-    return velocities;
+    return metrics;
+};
+
+export const getAvgVelocities = (set) => {
+    return getMetrics(set, RepDataMap.averageVelocity);
 };
 
 export const getPKVs = (set) => {
-    let pkvs = [];
-    
-    set.reps.forEach((rep) => {                
-        if (rep.isValid === true && rep.removed === false) {
-            let repData = rep.data;
-        
-            pkvs.push(Number(RepDataMap.peakVelocity(repData)));
-        }            
-    });
-
-    return pkvs;
+    return getMetrics(set, RepDataMap.peakVelocity);
 };
 
 export const getPKHs = (set) => {
-    let pkhs = [];
-    
-    set.reps.forEach((rep) => {                
-        if (rep.isValid === true && rep.removed === false) {
-            let repData = rep.data;
-        
-            pkhs.push(Number(RepDataMap.peakVelocityLocation(repData)));
-        }            
-    });
-
-    return pkhs;
+    return getMetrics(set, RepDataMap.peakVelocityLocation);
 };
 
 export const getROMs = (set) => {
-    let roms = [];
-    
-    set.reps.forEach((rep) => {                
-        if (rep.isValid === true && rep.removed === false) {
-            let repData = rep.data;
-        
-            roms.push(Number(RepDataMap.rangeOfMotion(repData)));
-        }            
-    });
-
-    return roms;
+    return getMetrics(set, RepDataMap.rangeOfMotion);
 };
 
 export const getDurations = (set) => {
-    let durations = [];
-    
-    set.reps.forEach((rep) => {                
-        if (rep.isValid === true && rep.removed === false) {
-            let repData = rep.data;
-        
-            durations.push(Number(RepDataMap.durationOfLift(repData)));
-        }            
-    });
-
-    return durations;
+    return getMetrics(set, RepDataMap.durationOfLift);
 };
 
 // Quantifiers
 
-export const getAvgofMetrics = (metrics) => {
-    
-    if (metrics.length > 0) {
-        let sum = metrics.reduce((previous, current) => current + previous);
-        return Number((sum / metrics.length).toFixed(2));
-    } else {
-        return null;
-    }  
-};
-    
-export const getAbsLossOfMetrics = (metrics) => {
-
-    if (metrics.length > 0) {
-        let maxV = Math.max(...metrics);
-        let minV = Math.min(...metrics);
-        
-        return maxV - minV;
-    } else {
+export const getAvgOfMetrics = (metrics) => {
+    if (metrics.length <= 0) {
         return null;
     }
+
+    const sum = metrics.reduce((previous, current) => current + previous);
+    return Number((sum / metrics.length).toFixed(2));
+};
+
+export const getAbsLossOfMetrics = (metrics) => {
+    if (metrics.length <= 0) {
+        return null;
+    }
+
+    const maxV = Math.max(...metrics);
+    const minV = Math.min(...metrics);
+    
+    return maxV - minV;
 };
 
 export const getFirstRepofMetrics = (metrics) => {
-    
-    if (metrics.length > 0) {
-        return metrics[0];
-    } else {
+    if (metrics.length <= 0) {
         return null;
     }
+
+    return metrics[0];
 };
 
 export const getLastRepMetrics = (metrics) => {
-        
-    if (metrics.length > 0) {
-        return metrics[metrics.length - 1];
-    } else {
+    if (metrics.length <= 0) {
         return null;
     }
+
+    return metrics[metrics.length - 1];
 };
 
 export const getMinMetrics = (metrics) => {
-    
-    if (metrics.length > 0) {
-        return Math.min(...metrics);
-    } else {
+    if (metrics.length <= 0) {
         return null;
     }
+
+    return Math.min(...metrics);
 };
 
 export const getMaxMetrics = (metrics) => {
-        
-    if (metrics.length > 0) {
-        return Math.max(...metrics);    
-    } else {
+    if (metrics.length <= 0) {
         return null;
     }
+
+    return Math.max(...metrics);
 };

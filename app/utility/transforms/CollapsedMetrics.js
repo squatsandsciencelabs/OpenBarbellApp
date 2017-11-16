c0caa3afa76b68bcb9244828a98aa9b55ee06c3b
Malek <mmalek1421@gmail.com>
2017-11-15 19:41:30 -0500

    refactor quanitifiers and metrics

--- app/utility/transforms/CollapsedMetrics.js
+++ app/utility/transforms/CollapsedMetrics.js
@@ -1,6 +1,8 @@
 import * as RepDataMap from 'app/utility/transforms/RepDataMap';
 
-export const getAvgVelocities = (set) => {
+// unique metrics
+
+export const getAvgVelocities = (set, metrics) => {
     const velocities = [];
     
     set.reps.forEach((rep) => {                
@@ -14,69 +16,6 @@ export const getAvgVelocities = (set) => {
     return velocities;
 };
 
-export const getAvgofAvgVelocities = (set) => {
-    const avgVs = getAvgVelocities(set);
-
-    if (avgVs.length > 0) {
-        let sum = avgVs.reduce((previous, current) => current + previous);
-        return Number((sum / avgVs.length).toFixed(2));
-    } else {
-        return null;
-    }  
-};
-
-export const getAbsLossVelocity = (set) => {
-    const avgVs = getAvgVelocities(set);
-
-    if (avgVs.length > 0) {
-        let maxV = Math.max(...avgVs);
-        let minV = Math.min(...avgVs);
-        
-        return maxV - minV;
-    } else {
-        return null;
-    }
-};
-
-export const getFirstRepAvgVelocity = (set) => {
-    const avgVs = getAvgVelocities(set);
-    
-    if (avgVs.length > 0) {
-        return avgVs[0];
-    } else {
-        return null;
-    }
-};
-
-export const getLastRepAvgVelocity = (set) => {
-    const avgVs = getAvgVelocities(set);
-    
-    if (avgVs.length > 0) {
-        return avgVs[avgVs.length - 1];
-    } else {
-        return null;
-    }
-};
-
-export const getMinAvgVelocity = (set) => {
-    const avgVs = getAvgVelocities(set);
-
-    if (avgVs.length > 0) {
-        return Math.min(...avgVs);
-    } else {
-        return null;
-    }
-};
-
-export const getMaxAvgVelocity = (set) => {
-    const avgVs = getAvgVelocities(set);
-    
-    if (avgVs.length > 0) {
-        return Math.max(...avgVs);    
-    } else {
-        return null;
-    }
-};
 
 export const getROMs = (set) => {
     let roms = [];
@@ -92,23 +31,24 @@ export const getROMs = (set) => {
     return roms;
 };
 
-export const getAvgofROMs = (set) => {
-    let roms = getROMs(set);
 
-    if (roms.length > 0) {
-        let sum = roms.reduce((previous, current) => current + previous);
-        return Number((sum / roms.length).toFixed(2));
+// Quantifiers
+
+export const getAvgofMetrics = (metrics) => {
+    
+    if (metrics.length > 0) {
+        let sum = metrics.reduce((previous, current) => current + previous);
+        return Number((sum / metrics.length).toFixed(2));
     } else {
         return null;
     }  
 };
+    
+export const getAbsLossOfMetrics = (metrics) => {
 
-export const getAbsLossROM = (set) => {
-    let roms = getROMs(set);
-
-    if (roms.length > 0) {
-        let maxV = Math.max(...roms);
-        let minV = Math.min(...roms);
+    if (metrics.length > 0) {
+        let maxV = Math.max(...metrics);
+        let minV = Math.min(...metrics);
         
         return maxV - minV;
     } else {
@@ -116,44 +56,38 @@ export const getAbsLossROM = (set) => {
     }
 };
 
-export const getFirstRepROM = (set) => {
-    let roms = getROMs(set);
+export const getFirstRepofMetrics = (metrics) => {
     
-    if (roms.length > 0) {
-        return roms[0];
+    if (metrics.length > 0) {
+        return metrics[0];
     } else {
         return null;
     }
 };
 
-export const getLastRepROM = (set) => {
-    let roms = getROMs(set);
+export const getLastRepMetrics = (metrics) => {
+        
+    if (metrics.length > 0) {
+        return metrics[metrics.length - 1];
+    } else {
+        return null;
+    }
+};
+
+export const getMinMetrics = (metrics) => {
     
-    if (roms.length > 0) {
-        return roms[roms.length - 1];
+    if (metrics.length > 0) {
+        return Math.min(...metrics);
     } else {
         return null;
     }
 };
 
-export const getMinROM = (set) => {
-    let roms = getROMs(set);
-
-    if (roms.length > 0) {
-        return Math.min(...roms);
+export const getMaxMetrics = (metrics) => {
+        
+    if (metrics.length > 0) {
+        return Math.max(...metrics);    
     } else {
         return null;
     }
 };
-
-export const getMaxROM = (set) => {
-    let roms = getROMs(set);
-    
-    if (roms.length > 0) {
-        return Math.max(...roms);    
-    } else {
-        return null;
-    }
-};
-
-// peak velocity

fcd0bc4b3ef39777aaea5fc9506f53ec38bfeeb0
JonnyLinja <jonny.linja@gmail.com>
2017-11-26 05:45:09 -0500

    aligned carrots in settings

--- app/features/settings/metrics/SettingsMetricsPanel.js
+++ app/features/settings/metrics/SettingsMetricsPanel.js
@@ -45,8 +45,9 @@ class SettingsMetricsPanel extends Component {
                 style={[SETTINGS_PANEL_STYLES.blueButton, {width: 100, height: 30, marginLeft: 10, marginBottom: 10}]}
                 onPress={() => this._tapCurrentMetric(row)}>
                 <Text style={SETTINGS_PANEL_STYLES.buttonText}>
-                    {CollapsedMetricsUtility.metricAbbreviation(metric)}  <Icon name="caret-down" size={10} color='white' />
+                    {CollapsedMetricsUtility.metricAbbreviation(metric)}
                 </Text>
+                <Icon name="caret-down" size={10} color='white' style={{right: 10, position:'absolute'}} />
             </TouchableOpacity>
         );
     }
@@ -57,8 +58,9 @@ class SettingsMetricsPanel extends Component {
                 style={[SETTINGS_PANEL_STYLES.blueButton, {width: 120, height: 30, marginLeft: 10, marginBottom: 10}]}
                 onPress={() => this._tapCurrentQuantifier(row)}>
                 <Text style={SETTINGS_PANEL_STYLES.buttonText}>
-                    {CollapsedMetricsUtility.quantifierString(quantifier)}  <Icon name="caret-down" size={10} color='white' />  
+                    {CollapsedMetricsUtility.quantifierString(quantifier)}
                 </Text>
+                <Icon name="caret-down" size={10} color='white' style={{right: 10, position:'absolute'}} />
             </TouchableOpacity>
         );
     }

dd37e17efce1fc1f46ca1e5d65eafb54ffea3164
JonnyLinja <jonny.linja@gmail.com>
2017-11-27 01:04:14 -0500

    Screens for collapsed settings metrics

--- app/features/settings/metrics/SettingsMetricsActions.js
+++ app/features/settings/metrics/SettingsMetricsActions.js
@@ -9,15 +9,27 @@ import {
 } from 'app/ActionTypes';
 import * as Analytics from 'app/services/Analytics';
 
-export const presentCollapsedMetric = (rank) => ({
-    type: PRESENT_COLLAPSED_METRIC,
-    metricRank: rank,
-});
+export const presentCollapsedMetric = (rank) => (dispatch, getState) => {
+    const state = getState();
 
-export const presentQuantifier = (rank) => ({
-    type: PRESENT_QUANTIFIER,    
-    quantifierRank: rank,
-});
+    Analytics.setCurrentScreen('edit_collapsed_metric');
+
+    dispatch({
+        type: PRESENT_COLLAPSED_METRIC,
+        metricRank: rank,
+    });
+};
+
+export const presentQuantifier = (rank) => (dispatch, getState) => {
+    const state = getState();
+
+    Analytics.setCurrentScreen('edit_quantifier');
+
+    dispatch({
+        type: PRESENT_QUANTIFIER,    
+        quantifierRank: rank,
+    });
+};
 
 export const presentBigMetricsInfo = () => (dispatch, getState) => {
     Linking.openURL("http://www.squatsandscience.com/metrics/");    

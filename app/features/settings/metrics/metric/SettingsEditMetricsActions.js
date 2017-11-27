2e30ca943efabcd28c2cabcf602ff90ffad7093f
JonnyLinja <jonny.linja@gmail.com>
2017-11-27 00:41:13 -0500

    Android MEtric Analytics

--- app/features/settings/metrics/metric/SettingsEditMetricsActions.js
+++ app/features/settings/metrics/metric/SettingsEditMetricsActions.js
@@ -9,29 +9,49 @@ import * as CollapsedSettingsSelectors from 'app/redux/selectors/CollapsedSettin
 export const saveCollapsedMetricSetting = (metric) => (dispatch, getState) => {
     const state = getState();
     const prevMetric = CollapsedSettingsSelectors.getCurrentMetric(state);
-
-    logChangeCollapsedMetricAnalytics(prevMetric, metric, state);
+    const rank = CollapsedSettingsSelectors.getCurrentCollapsedMetricRank(state);
+    logChangeCollapsedMetricAnalytics(rank, prevMetric, metric, state);
 
     dispatch(CollapsedSettingsActionCreators.saveCollapsedMetric(metric));
 };
 
 export const saveCollapsedMetricSetting1 = (metric) => (dispatch, getState) => {
+    const state = getState();
+    const prevMetric = CollapsedSettingsSelectors.getMetric1(state);
+    logChangeCollapsedMetricAnalytics(1, prevMetric, metric, state);
+
     dispatch(CollapsedSettingsActionCreators.saveCollapsedMetric1(metric));
 };
 
 export const saveCollapsedMetricSetting2 = (metric) => (dispatch, getState) => {
+    const state = getState();
+    const prevMetric = CollapsedSettingsSelectors.getMetric2(state);
+    logChangeCollapsedMetricAnalytics(2, prevMetric, metric, state);
+
     dispatch(CollapsedSettingsActionCreators.saveCollapsedMetric2(metric));
 };
 
 export const saveCollapsedMetricSetting3 = (metric) => (dispatch, getState) => {
+    const state = getState();
+    const prevMetric = CollapsedSettingsSelectors.getMetric3(state);
+    logChangeCollapsedMetricAnalytics(3, prevMetric, metric, state);
+
     dispatch(CollapsedSettingsActionCreators.saveCollapsedMetric3(metric));
 };
 
 export const saveCollapsedMetricSetting4 = (metric) => (dispatch, getState) => {
+    const state = getState();
+    const prevMetric = CollapsedSettingsSelectors.getMetric4(state);
+    logChangeCollapsedMetricAnalytics(4, prevMetric, metric, state);
+
     dispatch(CollapsedSettingsActionCreators.saveCollapsedMetric4(metric));
 };
 
 export const saveCollapsedMetricSetting5 = (metric) => (dispatch, getState) => {
+    const state = getState();
+    const prevMetric = CollapsedSettingsSelectors.getMetric5(state);
+    logChangeCollapsedMetricAnalytics(5, prevMetric, metric, state);
+
     dispatch(CollapsedSettingsActionCreators.saveCollapsedMetric5(metric));
 };
 
@@ -45,8 +65,9 @@ export const dismissCollapsedMetricSetter = () => {
 
 // ANALYTICS
 
-const logChangeCollapsedMetricAnalytics = (prevMetric, metric, state) => {
+const logChangeCollapsedMetricAnalytics = (rank, prevMetric, metric, state) => {
     Analytics.logEventWithAppState('change_collapsed_metric', {
+        rank: rank,
         from_metric: prevMetric,
         to_metric: metric,
     }, state);

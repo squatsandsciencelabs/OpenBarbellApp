79b305ca0ea8131c4761b9d8808a4b51b8910569
JonnyLinja <jonny.linja@gmail.com>
2017-11-27 00:39:28 -0500

    Android Quantifier Analytics

--- app/features/settings/metrics/quantifier/SettingsEditQuantifiersActions.js
+++ app/features/settings/metrics/quantifier/SettingsEditQuantifiersActions.js
@@ -8,30 +8,50 @@ import * as CollapsedSettingsSelectors from 'app/redux/selectors/CollapsedSettin
 
 export const saveQuantifierSetting = (quantifier) => (dispatch, getState) => {
     const state = getState();
-    
     const prevQuantifier = CollapsedSettingsSelectors.getCurrentQuantifier(state);
-    logChangeQuantifierAnalytics(prevQuantifier, quantifier, state);
+    const rank = CollapsedSettingsSelectors.getCurrentCollapsedMetricRank(state);
+    logChangeQuantifierAnalytics(rank, prevQuantifier, quantifier, state);
 
     dispatch(CollapsedSettingsActionCreators.saveQuantifier(quantifier));
 };
 
 export const saveQuantifier1Setting = (quantifier) => (dispatch, getState) => {
+    const state = getState();
+    const prevQuantifier = CollapsedSettingsSelectors.getQuantifier1(state);
+    logChangeQuantifierAnalytics(1, prevQuantifier, quantifier, state);
+
     dispatch(CollapsedSettingsActionCreators.saveQuantifier1(quantifier));
 };
 
 export const saveQuantifier2Setting = (quantifier) => (dispatch, getState) => {
+    const state = getState();
+    const prevQuantifier = CollapsedSettingsSelectors.getQuantifier2(state);
+    logChangeQuantifierAnalytics(2, prevQuantifier, quantifier, state);
+
     dispatch(CollapsedSettingsActionCreators.saveQuantifier2(quantifier));
 };
 
 export const saveQuantifier3Setting = (quantifier) => (dispatch, getState) => {
+    const state = getState();
+    const prevQuantifier = CollapsedSettingsSelectors.getQuantifier3(state);
+    logChangeQuantifierAnalytics(3, prevQuantifier, quantifier, state);
+
     dispatch(CollapsedSettingsActionCreators.saveQuantifier3(quantifier));
 };
 
 export const saveQuantifier4Setting = (quantifier) => (dispatch, getState) => {
+    const state = getState();
+    const prevQuantifier = CollapsedSettingsSelectors.getQuantifier4(state);
+    logChangeQuantifierAnalytics(4, prevQuantifier, quantifier, state);
+
     dispatch(CollapsedSettingsActionCreators.saveQuantifier4(quantifier));
 };
 
 export const saveQuantifier5Setting = (quantifier) => (dispatch, getState) => {
+    const state = getState();
+    const prevQuantifier = CollapsedSettingsSelectors.getQuantifier5(state);
+    logChangeQuantifierAnalytics(5, prevQuantifier, quantifier, state);
+
     dispatch(CollapsedSettingsActionCreators.saveQuantifier5(quantifier));
 };
 
@@ -45,8 +65,9 @@ export const dismissQuantifierSetter = () => {
 
 // ANALYTICS
 
-const logChangeQuantifierAnalytics = (prevQuantifier, quantifier, state) => {
+const logChangeQuantifierAnalytics = (rank, prevQuantifier, quantifier, state) => {
     Analytics.logEventWithAppState('change_quantifier', {
+        rank: rank,
         from_quantifier: prevQuantifier,
         to_quantifier: quantifier,
     }, state);

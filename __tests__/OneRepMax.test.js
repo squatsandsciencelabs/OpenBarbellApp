ea74a9c1fc00f2bf345b7a0872bc07c71a6b2ca3
Malek <mmalek1421@gmail.com>
2017-12-08 15:21:08 -0500

    added repmax and maxprediction functions

--- __tests__/OneRepMax.test.js
+++ __tests__/OneRepMax.test.js
@@ -2,9 +2,19 @@ import * as sut from 'app/utility/transforms/OneRepMax';
 
 describe('1rm', () => {
 
-    test('return yIntercept', () => {
-        const result = sut.calculate1RM();
+    test('return velocity estimate', () => {
+        const data = [
+            [255, 0.48], 
+            [275, 0.31], 
+            [285, 0.30], 
+            [295, 0.28], 
+            [300, 0.26], 
+            [310, 0.22], 
+            [320, 0.19]
+        ];
+
+        const result = sut.velocityPrediction(data, 330);
         
-        expect(result).toBe(0);
-    })
-})
+        expect(result).toBe(0.13);
+    });
+});

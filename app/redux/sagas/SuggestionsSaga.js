29906a5bdab55fe3320b8b0aae0ff8c42966749c
JonnyLinja <jonny.linja@gmail.com>
2017-12-08 21:04:44 -0500

    fixed exercise model and have it update every time you save exercises or tags

--- app/redux/sagas/SuggestionsSaga.js
+++ app/redux/sagas/SuggestionsSaga.js
@@ -1,18 +1,44 @@
-import { take, put } from 'redux-saga/effects';
+import { take, put, fork } from 'redux-saga/effects';
 import {
     LOGIN_SUCCESS,
     LOGOUT,
-    STORE_INITIALIZED
+    STORE_INITIALIZED,
+    SAVE_WORKOUT_SET,
+    SAVE_HISTORY_SET,
+    SAVE_WORKOUT_SET_TAGS,
+    SAVE_HISTORY_SET_TAGS,
 } from 'app/ActionTypes';
 
 import * as SuggestionsActionCreators from 'app/redux/shared_actions/SuggestionsActionCreators';
 
 const SuggestionsSaga = function * SuggestionsSaga() {
+    yield fork(exerciseSuggestions);
+    yield fork(tagSuggestions);
+    yield fork(bothSuggestions);
+};
+
+function* exerciseSuggestions() {
+    while (true) {
+        const action = yield take([SAVE_WORKOUT_SET, SAVE_HISTORY_SET]);
+        if ('exercise' in action) {
+            yield put(SuggestionsActionCreators.updateExerciseSuggestions());
+        }
+    }
+}
+
+function* tagSuggestions() {
+    while (true) {
+        yield take([SAVE_WORKOUT_SET_TAGS, SAVE_HISTORY_SET_TAGS]);
+        yield put(SuggestionsActionCreators.updateTagSuggestions());
+    }
+}
+
+function* bothSuggestions() {
     while (true) {
         yield take([LOGIN_SUCCESS, LOGOUT, STORE_INITIALIZED]);
         yield put(SuggestionsActionCreators.updateExerciseSuggestions());
         yield put(SuggestionsActionCreators.updateTagSuggestions());
     }
-};
+}
 
 export default SuggestionsSaga;

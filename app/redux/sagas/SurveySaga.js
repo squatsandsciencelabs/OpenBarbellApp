374996a9bd0024957ba07e126e4263c73c106954
JonnyLinja <jonny.linja@gmail.com>
2018-01-05 23:45:00 -0500

    Survey now checked on initialize and tab change

--- app/redux/sagas/SurveySaga.js
+++ app/redux/sagas/SurveySaga.js
@@ -1,16 +1,24 @@
-import { take, put, apply } from 'redux-saga/effects';
+import { takeEvery, put, apply, all } from 'redux-saga/effects';
 
 import { 
-    STORE_INITIALIZED
+    STORE_INITIALIZED,
+    CHANGE_TAB,
 } from 'app/ActionTypes';
 import firebase from 'app/services/Firebase';
 import * as SurveyActionCreators from 'app/redux/shared_actions/SurveyActionCreators';
 
 const SurveySaga = function * SurveySaga() {
-    yield take(STORE_INITIALIZED);
+    yield all([
+        takeEvery(STORE_INITIALIZED, updateSurveyURL),
+        takeEvery(CHANGE_TAB, updateSurveyURL),
+    ]);
+};
+
+function* updateSurveyURL() {
     const fbconfig = firebase.config();
     try {
-        yield apply(fbconfig, fbconfig.fetch, [0]); // TODO: remove the [0] when not debugging
+        // TODO: do we want to fetch this every time [0] or the default every 12 hours?
+        yield apply(fbconfig, fbconfig.fetch, [0]);
         const activated = yield apply(fbconfig, fbconfig.activateFetched);
         if (!activated) {
             console.tron.log("Fetched data not activated");
@@ -23,6 +31,6 @@ const SurveySaga = function * SurveySaga() {
         // TODO: analytics here for problems
         console.tron.log(JSON.stringify(error));
     }
-};
+}
 
 export default SurveySaga;

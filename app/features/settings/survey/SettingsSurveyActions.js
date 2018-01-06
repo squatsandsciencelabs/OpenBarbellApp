8fd21c213453cf42b04b882576907d98b80201b2
JonnyLinja <jonny.linja@gmail.com>
2018-01-06 15:07:27 -0500

    Added analytics for present survey

--- app/features/settings/survey/SettingsSurveyActions.js
+++ app/features/settings/survey/SettingsSurveyActions.js
@@ -1,10 +1,23 @@
 import {
     PRESENT_SURVEY,
 } from 'app/ActionTypes';
-
+import * as Analytics from 'app/services/Analytics';
 import * as SurveyActionCreators from 'app/redux/shared_actions/SurveyActionCreators';
+import * as SurveySelectors from 'app/redux/selectors/SurveySelectors';
 
 export const presentSurvey = () => (dispatch, getState) => {
+    // analytics
+    const state = getState();
+    logPresentSurveyAnalytics(state);
+
     // TODO: analytics
     dispatch(SurveyActionCreators.presentSurvey());
+};
+
+// ANALYTICS
+
+const logPresentSurveyAnalytics = (state) => {
+    Analytics.logEventWithAppState('present_survey', {
+        url: SurveySelectors.getURL(state),
+    }, state);
 };

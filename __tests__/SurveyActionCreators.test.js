b827a09f3c54ad27ffb1548d3e4072e50054ce43
JonnyLinja <jonny.linja@gmail.com>
2018-01-06 15:49:29 -0500

    analytics survey screen is set

--- __tests__/SurveyActionCreators.test.js
+++ __tests__/SurveyActionCreators.test.js
@@ -3,20 +3,18 @@ import thunk from 'redux-thunk';
 import configureMockStore from 'redux-mock-store';
 
 import * as Analytics from 'app/services/Analytics';
-import * as sut from 'app/features/settings/survey/SettingsSurveyActions';
+import * as sut from 'app/redux/shared_actions/SurveyActionCreators';
 
 const middlewares = [ thunk ];
 const mockStore = configureMockStore(middlewares);
-var logEventSpy = null;
 var logScreenSpy = null;
 var store = null;
 
-describe('SettingsSurveyActions', () => {
+describe('SettingsActionCreators', () => {
 
     const expectedURL = 'foobar';
 
     beforeEach(() => {
-        logEventSpy = jest.spyOn(Analytics, 'logEventWithAppState').mockImplementation(() => {});
         logScreenSpy = jest.spyOn(Analytics, 'setCurrentScreen').mockImplementation(() => {});        
         store = mockStore({
             survey: {
@@ -26,22 +24,18 @@ describe('SettingsSurveyActions', () => {
     });
 
     afterEach(() => {
-        logEventSpy.mockReset();
-        logScreenSpy.mockReset();
+        logScreenSpy.mockReset();        
     });
 
     afterAll(() => {
-        logEventSpy.mockRestore();
         logScreenSpy.mockRestore();
     });   
 
-    test('log url when presenting', () => {
+    test('set analytics screen when presenting', () => {
         store.dispatch(sut.presentSurvey());
         
-        const event = logEventSpy.mock.calls[0][0];
-        const params = logEventSpy.mock.calls[0][1];
-        expect(event).toEqual('present_survey');
-        expect(params.url).toEqual(expectedURL);
+        const screen = logScreenSpy.mock.calls[0][0];
+        expect(screen).toEqual('survey');
     });
 
 });

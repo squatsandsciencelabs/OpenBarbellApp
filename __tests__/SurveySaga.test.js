2cd01350356bd0e4783c48b89acb767aee824d6c
JonnyLinja <jonny.linja@gmail.com>
2018-01-06 18:31:04 -0500

    giving up on tests for sagas for now, have notes intsead

---
+++ __tests__/SurveySaga.test.js
@@ -0,0 +1,36 @@
+// jest.mock('app/services/Firebase', () => {});
+
+// import * as sut from 'app/redux/sagas/SurveySaga';
+
+// TODO: figure out how to properly test sagas
+// some suggest libraries, this is getting too big so just manual test it for now
+// https://redux-saga.js.org/docs/advanced/Testing.html
+// https://github.com/Gethyl/FirstReduxSagaExample
+// https://stackoverflow.com/questions/48033184/how-to-test-redux-saga-with-jest
+
+describe('SurveySaga Analytics', () => {
+
+    test.skip('update_survey_url', () => {
+    });
+
+    test.skip('update_survey_url_error inactive', () => {
+
+    });
+
+    test.skip('update_survey_url_error general', () => {
+
+    });
+
+    test.skip('prompt_survey', () => {
+
+    });
+
+    test.skip('prompt_survey_fill_later', () => {
+
+    });
+
+    test.skip('prompt_survey_take_now', () => {
+
+    });
+
+});

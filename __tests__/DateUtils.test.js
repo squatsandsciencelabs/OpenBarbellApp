b076f23ee7163122da1de2ae746e080a91a0b223
JonnyLinja <jonny.linja@gmail.com>
2018-01-12 16:50:26 -0500

    Updated test cases

--- __tests__/DateUtils.test.js
+++ __tests__/DateUtils.test.js
@@ -5,23 +5,23 @@ describe('DateUtils', () => {
         const realNow = Date.now;
 
         beforeAll(() => {
-            Date.now = () => 1515197192603;
+            Date.now = () => 1515197192603; // Saturday, January 6, 2018
         })
 
         afterAll(() => {
             Date.now = realNow;
         });
 
-        test('check if date is within range', () => {  
-            const date = '2017-12-30T04:06:12.640Z';
+        test('check if date is within 7 days', () => {  
+            const date = '2018-01-12T04:06:12.640Z';
 
             const actual = sut.checkDateWithinRange(7, date)
     
             expect(actual).toBe(true);
         });
 
-        test('check if date is within range over 7 days', () => {  
-            const date = '2017-12-27T04:06:12.640Z';
+        test('check if date is within range over 10 days', () => {  
+            const date = '2018-01-15T04:06:12.640Z';
 
             const actual = sut.checkDateWithinRange(10, date)
     
@@ -29,7 +29,7 @@ describe('DateUtils', () => {
         });
 
         test('check if works with date object', () => {  
-            const dateString = '2017-12-30T04:06:12.640Z';
+            const dateString = '2018-01-12T04:06:12.640Z';
             const date = new Date(dateString);
 
             const actual = sut.checkDateWithinRange(7, date)
@@ -37,13 +37,21 @@ describe('DateUtils', () => {
             expect(actual).toBe(true);
         });
 
-
-        test('check if date is out of range', () => {    
-            const date = '2017-12-05T04:06:12.640Z';
+        test('check if date is greater than 7 days', () => {    
+            const date = '2018-01-14T04:06:12.640Z';
 
             const actual = sut.checkDateWithinRange(7, date)
     
             expect(actual).toBe(false);
         });
+
+        test('check if date is within range over 10 days', () => {  
+            const date = '2018-01-17T04:06:12.640Z';
+
+            const actual = sut.checkDateWithinRange(10, date)
+    
+            expect(actual).toBe(false);
+        });
+
     });
 });

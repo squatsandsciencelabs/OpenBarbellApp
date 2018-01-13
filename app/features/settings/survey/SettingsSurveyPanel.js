0e3a45545cce1a000855cfa8b3bbfe753d7fa520
JonnyLinja <jonny.linja@gmail.com>
2018-01-13 00:22:37 -0500

    Adam shows up now in Settings

--- app/features/settings/survey/SettingsSurveyPanel.js
+++ app/features/settings/survey/SettingsSurveyPanel.js
@@ -3,7 +3,8 @@ import {
     Text,
     View,
     TouchableOpacity,
-    StyleSheet
+    StyleSheet,
+    Image,
 } from 'react-native';
 
 import { SETTINGS_PANEL_STYLES } from 'app/appearance/styles/GlobalStyles';
@@ -14,12 +15,17 @@ class SettingsSurveyPanel extends Component {
         if (this.props.isVisible) {
             return (
                 <View style={ [SETTINGS_PANEL_STYLES.panel, { flexDirection: 'column' }] }>
-                    <Text style={[{marginBottom: 20}, styles.titleText]}>Help us out, take our survey!</Text>
+                    <View style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
+                        <Image style={styles.avatar} source={require('app/appearance/images/adam.png')} />
+                        <View style={{flex: 1, marginLeft: 20}}>
+                            <Text style={[styles.titleText]}>This is Adam, he does our data science.</Text>
+                        </View>
+                    </View>
 
-                    <TouchableOpacity style={[SETTINGS_PANEL_STYLES.blueButton, {height: 50}]}
+                    <TouchableOpacity
                         disabled={ false }
                         onPress={ () => this.props.presentSurvey() }>
-                            <Text style={SETTINGS_PANEL_STYLES.buttonText}>TAKE SURVEY</Text>
+                            <Text style={[SETTINGS_PANEL_STYLES.tappableText, {marginTop: 15}]}>Take a quick survey so he can help us understand your data.</Text>
                     </TouchableOpacity>
 
                 </View>
@@ -32,10 +38,15 @@ class SettingsSurveyPanel extends Component {
 }
 
 const styles = StyleSheet.create({
+    avatar: {
+        height: 80,
+        width: 80,
+    },
     titleText: {
         color: 'rgba(77, 77, 77, 1)',
-        textAlign: 'center',
-        fontSize: 20,
+        textAlign: 'left',
+        fontSize: 17,
+        fontWeight: '700',
     },
 });
 

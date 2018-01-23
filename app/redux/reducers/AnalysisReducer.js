4a7a1da488ba538b857858d2ffccecc7efceb535
JonnyLinja <jonny.linja@gmail.com>
2018-01-23 16:04:36 -0500

    Semicolons

--- app/redux/reducers/AnalysisReducer.js
+++ app/redux/reducers/AnalysisReducer.js
@@ -35,7 +35,7 @@ const AnalysisReducer = (state = defaultState, action) => {
             return {
                 ...state,
                 velocitySlider: Number(action.velocity.toFixed(2)),
-            }
+            };
         case CHANGE_1RM_DAYS_RANGE: 
             return Object.assign({}, state, {
                 e1RMDaysRange: action.days,
@@ -47,7 +47,7 @@ const AnalysisReducer = (state = defaultState, action) => {
         case DISMISS_SELECT_EXERCISE:
             return Object.assign({}, state, {
                 isEditing1RMExercise: false,
-            })
+            });
         case SAVE_1RM_EXERCISE:
             return Object.assign({}, state, {
                 e1RMExercise: action.exercise,
@@ -56,39 +56,39 @@ const AnalysisReducer = (state = defaultState, action) => {
             return {
                 ...state,
                 isEditingIncludeTags: true,
-            }
+            };
         case SAVE_INCLUDES_TAGS:
             return {
                 ...state,
                 tagsToInclude: action.tags,
-            }
+            };
         case DISMISS_INCLUDES_TAGS:
             return {
                 ...state,
                 isEditingIncludeTags: false,
-            }
+            };
         case PRESENT_EXCLUDES_TAGS:
             return {
                 ...state,
                 isEditingExcludeTags: true,
-            }
+            };
         case SAVE_EXCLUDES_TAGS:
             return {
                 ...state,
                 tagsToExclude: action.tags,
-            }    
+            };
         case DISMISS_EXCLUDES_TAGS:
             return {
                 ...state,
                 isEditingExcludeTags: false,
-            }
+            };
         case CALC_ONE_RM:
             return {
                 ...state,
                 e1rm: action.e1rm,
                 e1RMVelocity: action.e1RMVelocity,
                 confidence: action.confidence,
-            }
+            };
         default: 
             return state;
     }

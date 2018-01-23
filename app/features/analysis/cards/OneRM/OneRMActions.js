22bc8283bf9afa192d1fab94eb0700f8f8e8a389
JonnyLinja <jonny.linja@gmail.com>
2018-01-23 14:52:35 -0500

    added commas

--- app/features/analysis/cards/OneRM/OneRMActions.js
+++ app/features/analysis/cards/OneRM/OneRMActions.js
@@ -7,17 +7,17 @@ import {
 } from 'app/ActionTypes';
 
 export const presentSelectExercise = () => ({
-    type: PRESENT_SELECT_EXERCISE
+    type: PRESENT_SELECT_EXERCISE,
 });
 
 export const changeE1RMVelocity = (velocity) => ({
     type: CHANGE_1RM_VELOCITY,
-    velocity: velocity
+    velocity: velocity,
 });
 
 export const changeE1RMDays = (days) => ({
     type: CHANGE_1RM_DAYS_RANGE,
-    days: Math.abs(days)
+    days: Math.abs(days),
 });
 
 export const presentTagsToExclude = () => ({

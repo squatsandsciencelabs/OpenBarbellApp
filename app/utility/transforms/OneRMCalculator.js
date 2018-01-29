9b25fc258e8a1087657a0056a5faf80519c4ff42
Malek <mmalek1421@gmail.com>
2018-01-29 17:10:47 -0500

    fixed offset and using regression points

--- app/utility/transforms/OneRMCalculator.js
+++ app/utility/transforms/OneRMCalculator.js
@@ -12,7 +12,7 @@ export const calc1rm = (data, velocity) => {
     // prediction
     // solving for x instead of y (x being weight, y being vel)
     return Number(((velocity - result.equation[1]) / result.equation[0]).toFixed(0));
-}
+};
 
 export const getConfidenceInterval = (data) => {
     // gather points
@@ -21,4 +21,25 @@ export const getConfidenceInterval = (data) => {
 
     // r2 prediction
     return Number((result.r2 * 100).toFixed(0));
+};
+
+export const calcVel = (data, weight) => {
+    if (!data) {
+        return null;
+    }
+
+    const result = regression.linear(data, { precision: 4 });
+
+    return result.predict(weight);
 }
+
+export const getRegressionPoints = (data) => {
+    if (!data) {
+        return null;
+    }
+    // gather points
+    // precision is 4 for accuracy of regression points in the library
+    const result = regression.linear(data, { precision: 4 });
+    
+    return result.points;
+};

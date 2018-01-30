02d4d32c3e67e3faca5f6b89749f2691a8b51b6e
JonnyLinja <jonny.linja@gmail.com>
2018-01-29 21:41:28 -0500

    Added semicolons

--- app/redux/selectors/SetsSelectors.js
+++ app/redux/selectors/SetsSelectors.js
@@ -456,7 +456,7 @@ export const getChartData = (state, exercise) => {
     });
 
     return data;
-}
+};
 
 export const getRegLinePoints = (state, exercise) => {
     const exerciseData = getExerciseData(state, exercise);

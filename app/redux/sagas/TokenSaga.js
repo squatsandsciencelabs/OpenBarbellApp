6a6051a5315b1e9d7481700dbad526ac8eecb52a
JonnyLinja <jonny.linja@gmail.com>
2018-01-30 01:35:49 -0500

    Fixed issues, proved it can reauthenticate its tokens. Theoretically this system allows us to have MUCH smaller tokens windows for security

--- app/redux/sagas/TokenSaga.js
+++ app/redux/sagas/TokenSaga.js
@@ -61,7 +61,7 @@ function* obtainNewTokens() {
             logRefreshTokenErrorAnalytics(state, error, refreshToken);
             if (error.type === '401') {
                 // request a sign in because the tokens can't be refreshed
-                dispatch(AuthActionCreators.requestLogin());
+                yield put(AuthActionCreators.requestLogin());
             } else if (error.type !== undefined || typeof error === 'function') {
                 // unknown error type, just propogate it
                 yield put(error);

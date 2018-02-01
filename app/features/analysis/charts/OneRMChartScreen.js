9d4e2817e50df5acb5b1bc71e9ac435b56372716
Malek <mmalek1421@gmail.com>
2018-02-01 16:53:14 -0500

    fixed calculate button bug on android

--- app/features/analysis/charts/OneRMChartScreen.js
+++ app/features/analysis/charts/OneRMChartScreen.js
@@ -3,6 +3,20 @@ import { connect } from 'react-redux';
 
 import OneRMChartView from './OneRMChartView';
 import * as Actions from './OneRMChartActions';
+import * as SetsSelectors from 'app/redux/selectors/SetsSelectors';
+import * as SettingsSelectors from 'app/redux/selectors/SettingsSelectors';
+import * as AnalysisSelectors from 'app/redux/selectors/AnalysisSelectors';
+import * as AuthSelectors from 'app/redux/selectors/AuthSelectors';
+import * as AppStateActionCreators from 'app/redux/shared_actions/AppStateActionCreators';
+import * as OneRMCalculator from 'app/utility/transforms/OneRMCalculator';
+
+const mapStateToProps = (state) => ({
+    chartData: AnalysisSelectors.getChartData(state),
+    regLineData: AnalysisSelectors.getRegLineData(state),
+    confidence: AnalysisSelectors.getCurrentConfidence(state),
+    minConfidence: 90, // TODO: make this a config
+    isLoggedIn: AuthSelectors.getIsLoggedIn(state),
+});
 
 const mapDispatchToProps = (dispatch) => {
     return bindActionCreators({
@@ -11,7 +25,7 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 const OneRMChartScreen = connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
 )(OneRMChartView);
 
--- app/features/analysis/charts/OneRMChartView.js
+++ app/features/analysis/charts/OneRMChartView.js
@@ -12,6 +12,8 @@ import {
   VictoryGroup, 
   VictoryLine 
 } from "victory-native";
+import * as Device from 'app/utility/Device';
+import { SETTINGS_PANEL_STYLES } from 'app/appearance/styles/GlobalStyles';
 
 class OneRMChartView extends Component {
     
@@ -35,36 +37,80 @@ class OneRMChartView extends Component {
       }
     } 
 
-    render() {
+  _renderChartArea() {
+    if (this.props.isLoggedIn) {
+      if (this.props.chartData && this.props.chartData.length > 3) {
         return (
-          <View style={{ alignItems: 'center' }}>
-            <VictoryChart
-              theme={VictoryTheme.material}
-            >
-              <VictoryGroup offset={0}
-                colorScale={"qualitative"}
-              >
-                {this._renderRegressionLine()}
-                <VictoryScatter
-                  style={{ data: { fill: "#c43a31" } }}
-                  width={400}
-                  size={5}
-                  // events={[{
-                  //   target: "data",
-                  //   eventHandlers: {
-                  //     onPressIn: (evt, clickedProps) => {
-                  //       const setID = clickedProps.datum.setID;
-                  //       this.props.tapPoint(setID);
-                  //     }
-                  //   }
-                  // }]}
-                  data={this.props.data}
-                />
-              </VictoryGroup>
-            </VictoryChart>
+          <View>
+            {this._renderChart()}
           </View>
         );
+      } else {
+        return;
+      }
+    } else {
+      const size = Device.isSmallDevice() ? 250 : 300;
+
+      return <Image style={{width: size, height: size, marginTop: 20}} source={require('app/appearance/images/grayed_chart.png')} />
     }
+  }
+
+  _renderChart() {
+    return (
+      <View>
+        <VictoryChart
+          theme={VictoryTheme.material}
+        >
+          <VictoryGroup offset={0}
+            colorScale={"qualitative"}
+          >
+            {this._renderRegressionLine()}
+            <VictoryScatter
+              style={{ data: { fill: "#c43a31" } }}
+              width={400}
+              size={5}
+              // events={[{
+              //   target: "data",
+              //   eventHandlers: {
+              //     onPressIn: (evt, clickedProps) => {
+              //       const setID = clickedProps.datum.setID;
+              //       this.props.tapPoint(setID);
+              //     }
+              //   }
+              // }]}
+              data={this.props.chartData}
+            />
+          </VictoryGroup>
+        </VictoryChart>
+      </View>
+    );      
+  }
+
+  render() {
+    return (
+      <View style={ [SETTINGS_PANEL_STYLES.panel, { flexDirection: 'column', borderBottomWidth: 0, alignItems: 'center' }] }>
+        <Text style={[{marginBottom: 15}, styles.titleText]}>Estimated One-Rep Max</Text>
+        {this._renderChartArea()}
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  titleText: {
+      color: 'rgba(77, 77, 77, 1)',
+      textAlign: 'center',
+      fontSize: 16,
+      fontWeight: 'bold'
+  },
+  pseudoScrollView: {
+    opacity: 0,
+    position: 'absolute',
+    top: 0,    
+    left: 0,
+    width:"100%",
+    height:"100%"
+  }
+});
 
 export default OneRMChartView;

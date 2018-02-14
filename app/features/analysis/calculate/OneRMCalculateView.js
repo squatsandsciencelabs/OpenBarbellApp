d12b091556370c233a815be2b085ca088afe1d65
Malek <mmalek1421@gmail.com>
2018-02-14 15:23:40 -0500

    adjusted drop down for android

--- app/features/analysis/calculate/OneRMCalculateView.js
+++ app/features/analysis/calculate/OneRMCalculateView.js
@@ -126,7 +126,7 @@ class OneRMView extends Component {
         } else {
             return (
                 <View>
-                    <ExercisePicker />  
+                    <View style={[{flex: 0.37}, styles.dropdownButton]}><ExercisePicker color={'white'} /></View>
                     <Text style={{textAlign: 'center', fontSize: 15, marginBottom: 10}}>Tags to Include:</Text>
                     <View style={{marginBottom: 10}}>{ this._renderTagsToInclude() }</View>
                     <Text style={{textAlign: 'center', fontSize: 15, marginBottom: 10}}>Tags to Exclude:</Text>

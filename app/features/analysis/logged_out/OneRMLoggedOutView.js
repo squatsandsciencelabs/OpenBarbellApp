54916bd8096e0184bdf64f113dbd5acb389e1952
Malek <mmalek1421@gmail.com>
2018-02-14 16:54:13 -0500

    new logged out screen

--- app/features/analysis/logged_out/OneRMLoggedOutView.js
+++ app/features/analysis/logged_out/OneRMLoggedOutView.js
@@ -6,6 +6,7 @@ import {
     Image,
  }  from 'react-native';
  import * as Device from 'app/utility/Device';
+ import { SETTINGS_PANEL_STYLES } from 'app/appearance/styles/GlobalStyles';
 
  class OneRMLoggedOutView extends Component {
 
@@ -13,14 +14,12 @@ import {
         const size = Device.isSmallDevice() ? 250 : 300;
 
         return (
-            <View>
+            <View style={ [SETTINGS_PANEL_STYLES.panel, { flexDirection: 'column', alignItems: 'center' }] }>
+                <Text style={[{marginBottom: 15}, styles.titleText]}>Estimated One-Rep Max</Text>
                 <Text style={styles.errorText}>
                     You must be logged in for 1rm calculation.
                 </Text>
                 <Image style={{width: size, height: size, marginTop: 20}} source={require('app/appearance/images/grayed_chart.png')} />
-                <View style={[styles.disabledButton, {marginTop: 20}]}>
-                    <Text style={styles.buttonText}>Calculate</Text>
-                </View>
             </View>
         )
     }
@@ -28,6 +27,12 @@ import {
  }
 
  const styles = StyleSheet.create({
+    titleText: {
+        color: 'rgba(77, 77, 77, 1)',
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
     disabledButton: {
         backgroundColor: 'rgba(47, 128, 237, 1)',
         borderColor: 'rgba(47, 128, 237, 1)',        
@@ -38,6 +43,12 @@ import {
     buttonText: {
         color: 'white',
         padding: 5,
+        textAlign: 'center'
+    },
+    errorText: {
+        color: 'rgba(77, 77, 77, 1)',
+        marginBottom: 20, 
+        fontSize: 20, 
         textAlign: 'center'
     },
 });

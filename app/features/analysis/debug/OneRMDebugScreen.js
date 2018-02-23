8f97521b94f55e31c722de1de821a5bb80d9adc0
JonnyLinja <jonny.linja@gmail.com>
2018-02-23 03:26:04 -0500

    now debug only shows up if its in debug mode

--- app/features/analysis/debug/OneRMDebugScreen.js
+++ app/features/analysis/debug/OneRMDebugScreen.js
@@ -7,7 +7,7 @@ import * as Actions from './OneRMDebugActions';
 const mapStateToProps = (state) => {
     // TODO: set this to false to remove it from the live production app
     return {
-        visible: true,
+        visible: __DEV__,
     };
 };
 

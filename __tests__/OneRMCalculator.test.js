265cb96417d3d05272ef8c65b440088b57e3efee
Malek <mmalek1421@gmail.com>
2018-02-23 15:03:54 -0500

    added basic state to 1rm test

--- __tests__/OneRMCalculator.test.js
+++ __tests__/OneRMCalculator.test.js
@@ -16,100 +16,24 @@ describe('1RM', () => {
     const state = {
         sets: {
             workoutData: [
-            {
-                setID: 'a',
-                exercise: 'Bench',
-                weight: 100,
-                metric: 'lbs',
-                reps: [{
-                    isValid: true,
-                    removed: false,
-                    data: [-3456, 37, 1.833368, 200, 1, 10, 1, 4]
-                }, {
-                    isValid: false,
-                    removed: false,
-                    data: [-3456, 37, 10.533368, 500, 2, 80, 1, 70]
-                }, {
-                    isValid: true,
-                    removed: false,
-                    data: [-3456, 37, 2.433368, 250, 3, 15, 1, 10]
-                }, {
-                    isValid: true,
-                    removed: true,
-                    data: [-3456, 37, 0.533368, 50, 4, 20, 1, 2]
-                }, {
-                    isValid: true,
-                    removed: false,
-                    data: [-3456, 37, 0.233368, 400, 5, 30, 1, 20]
-                }],
-                tags: ['A', 'B', 'C'],
-                initialStartTime: '1-2-18'
-            },
-            {
-                setID: 'h',
-                exercise: 'Squat',
-                weight: 100,
-                metric: 'lbs',
-                reps: [{
-                    isValid: true,
-                    removed: false,
-                    data: [-3456, 37, 1.333368, 388, 6, 24, 9, 12]
-                }, {
-                    isValid: true,
-                    removed: false,
-                    data: [-3456, 37, 1.033368, 378, 7, 69, 13, 8]
-                }, {
-                    isValid: true,
-                    removed: false,
-                    data: [-3456, 37, 1.453368, 328, 8, 72, 11, 15]
-                }],
-                tags: ['D', 'A', 'B', 'C'],
-                initialStartTime: '2018-01-03T04:06:12.640Z'
-            }],
+                {},
+                {},
+                {},
+                {},
+                {},
+            ],
             historyData: {
-                b: {
-                    setID: 'b',
-                    exercise: 'Bench',
-                    weight: 200,
-                    metric: 'lbs',
-                    reps: [{
-                        isValid: true,
-                        removed: false,
-                        data: [-3456, 37, 1.943368, 388, 38, 28, 1, 18]
-                    }],
-                    tags: ['A', 'G', 'B', 'C', 'F'],
-                    initialStartTime: '2018-01-03T04:06:12.640Z'
-                },
-                c: {
-                    setID: 'c',
-                    exercise: 'Bench',
-                    weight: 150,
-                    metric: 'lbs',
-                    reps: [{
-                        isValid: true,
-                        removed: false,
-                        data: [-3456, 37, 1.733368, 288, 8, 18, 1, 8]
-                    }],
-                    tags: ['A', 'E', 'B', 'C'],
-                    initialStartTime: '2018-01-03T04:06:12.640Z'
-                },
-                d: {
-                    setID: 'd',
-                    exercise: 'Bench',
-                    weight: 200,
-                    metric: 'lbs',
-                    reps: [{
-                        isValid: true,
-                        removed: false,
-                        data: [-3456, 37, 1.833368, 188, 3, 8, 1, 4]
-                    }],
-                    tags: ['A', 'B', 'F', 'C'],
-                    initialStartTime: '2018-01-03T04:06:12.640Z'
-                },
+                a: {},
+                b: {},
+                c: {},
+                d: {},
+                e: {},
+                f: {},
+                g: {}
             }
         },
         analysis: {
-            exercise: 'Bench',
+            exercise: 'Squat',
             daysRange: 7,
             velocitySlider: .15,
             tagsToInclude: ['A', 'B', 'C'],
@@ -119,6 +43,37 @@ describe('1RM', () => {
             metric: 'lbs',
         }
     };
+
+    test('return null if no data', () => {
+        const state = {
+            sets: {
+                workoutData: [],
+                historyData: {}
+            },
+            analysis: {
+                exercise: 'Bench',
+                daysRange: 7,
+                velocitySlider: .15,
+                tagsToInclude: ['A', 'B', 'C'],
+                tagsToExclude: ['F', 'G'],
+            },
+            settings: {
+                metric: 'lbs',
+            }
+        };
+            
+        const exercise = AnalysisSelectors.getExercise(state);
+        const tagsToInclude = AnalysisSelectors.getTagsToInclude(state);
+        const tagsToExclude = AnalysisSelectors.getTagsToExclude(state);
+        const daysRange = AnalysisSelectors.getDaysRange(state);
+        const velocity = AnalysisSelectors.getVelocitySlider(state);
+        const allSets = SetsSelectors.getAllSets(state);
+        const metric = SettingsSelectors.getDefaultMetric(state);
+
+        const results = sut.calculate1RM(exercise, tagsToInclude, tagsToExclude, daysRange, velocity, metric, allSets);
+
+        expect(results).toEqual({"active": [], "e1RM": null, "errors": [], "r2": null, "regressionPoints": [], "unused": []});
+    });
 
     // Test that proper pool is returned
     // Test that proper velocitys are placed in proper pools
@@ -137,7 +92,5 @@ describe('1RM', () => {
         const metric = SettingsSelectors.getDefaultMetric(state);
 
         const results = sut.calculate1RM(exercise, tagsToInclude, tagsToExclude, daysRange, velocity, metric, allSets);
-
-        expect(results).toEqual({"active": [{"setID": "c", "size": 10, "x": 68, "y": 1.73}], "e1RM": -Infinity, "errors": [{"setID": "a", "size": 10, "x": 45, "y": 2.43}], "r2": NaN, "regressionPoints": [{"x": 68, "y": 1.73}], "unused": []});
-    })
+    });
 });

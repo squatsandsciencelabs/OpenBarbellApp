c15b6f8a08ef371644559617d9c7a53bbe1400af
JonnyLinja <jonny.linja@gmail.com>
2018-02-25 05:00:16 -0500

    begun one_rm_close_edit_set analytics

--- app/features/analysis/edit_set/OneRMEditSetActions.js
+++ app/features/analysis/edit_set/OneRMEditSetActions.js
@@ -2,15 +2,26 @@ import {
     DISMISS_EDIT_1RM_SET,
 } from 'app/configs+constants/ActionTypes';
 import * as SetsActionCreators from 'app/redux/shared_actions/SetsActionCreators';
+import * as Analytics from 'app/services/Analytics';
 
 export const removeRep = (setID, repIndex) => SetsActionCreators.removeHistoryRep(setID, repIndex);
 
 export const restoreRep = (setID, repIndex) => SetsActionCreators.restoreHistoryRep(setID, repIndex);
 
-export const dismissEditSet = () => {
-    // TODO: analytics
+export const dismissEditSet = () => (dispatch, getState) => {
+    const state = getState();
+    logCloseEditSetAnalytics(state);
+    Analytics.setCurrentScreen('analysis');
 
-    return {
+    dispatch({
         type: DISMISS_EDIT_1RM_SET
-    };
+    });
+};
+
+// ANALYTICS
+
+const logCloseEditSetAnalytics = (state) => {
+    // TODO: this has a bunch
+    Analytics.logEventWithAppState('one_rm_close_edit_set', {
+    }, state);
 };

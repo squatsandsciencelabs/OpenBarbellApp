386d6b6518ffb2f3ff835aceb721a948c2e241f9
JonnyLinja <jonny.linja@gmail.com>
2018-02-25 03:10:55 -0500

    1rm exclude tag analytics

--- app/features/analysis/calculate/tags/tagsToExclude/EditAnalysisTagsToExcludeActions.js
+++ app/features/analysis/calculate/tags/tagsToExclude/EditAnalysisTagsToExcludeActions.js
@@ -4,20 +4,59 @@ import {
     SAVE_EXCLUDES_TAGS,
     DISMISS_EXCLUDES_TAGS,
 } from 'app/configs+constants/ActionTypes';
+import * as Analytics from 'app/services/Analytics';
 
-export const saveTags = (tags = []) => ({
-    type: SAVE_EXCLUDES_TAGS,
-    tags: tags,
-});
+export const saveTags = (tags = []) => (dispatch, getState) => {
+    const state = getState();
+    logSaveTagsAnalytics(state);
+    Analytics.setCurrentScreen('one_rm_edit_set');
 
-export const cancelTags = () => ({
-    type: DISMISS_EXCLUDES_TAGS,
-})
+    dispatch({
+        type: SAVE_EXCLUDES_TAGS,
+        tags: tags,
+    });
+};
 
-export const tappedPill = () => ({
-    type: REMOVE_EXCLUDE_TAG,
-});
+export const cancelTags = () => (dispatch, getState) => {
+    const state = getState();
+    logCancelEditTagsAnalytics(state);
+    Analytics.setCurrentScreen('one_rm_edit_set');
 
-export const addPill = () => ({
-    type: ADD_EXCLUDE_TAG,
-});
+    dispatch({ type: DISMISS_EXCLUDES_TAGS });
+};
+
+export const tappedPill = () => (dispatch, getState) => {
+    const state = getState();
+    logRemovedTagAnalytics(state);
+
+    dispatch({ type: REMOVE_EXCLUDE_TAG });
+};
+
+export const addPill = () => (dispatch, getState) => {
+    const state = getState();
+    logAddTagAnalytics(state);
+
+    dispatch({ type: ADD_EXCLUDE_TAG });
+};
+
+// ANALYTICS
+
+const logSaveTagsAnalytics = (state) => {
+    Analytics.logEventWithAppState('one_rm_select_exclude_tags', {
+    }, state);
+};
+
+const logCancelEditTagsAnalytics = (state) => {
+    Analytics.logEventWithAppState('one_rm_cancel_edit_exclude_tags', {
+    }, state);
+};
+
+const logRemovedTagAnalytics = (state) => {
+    Analytics.logEventWithAppState('remove_tag', {
+    }, state);
+};
+
+const logAddTagAnalytics = (state) => {
+    Analytics.logEventWithAppState('one_rm_add_tag', {
+    }, state);
+};

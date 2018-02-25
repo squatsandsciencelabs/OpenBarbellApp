0d89cb6915c3fe788561aaf9ce06e899bb534689
JonnyLinja <jonny.linja@gmail.com>
2018-02-25 03:44:46 -0500

    cancel and save 1rm exercise analytics

--- app/features/analysis/calculate/exercise/ExercisePickerActions.js
+++ app/features/analysis/calculate/exercise/ExercisePickerActions.js
@@ -5,12 +5,30 @@ import * as Analytics from 'app/services/Analytics';
 import * as SetsActionCreators from 'app/redux/shared_actions/SetsActionCreators';
 import * as AnalysisActionCreators from 'app/redux/shared_actions/AnalysisActionCreators';
 
-export const dismissSelectExercise = () => ({
-    type: DISMISS_SELECT_EXERCISE 
-});
+export const dismissSelectExercise = () => (dispatch, getState) => {
+    const state = getState();
+    logCancelEditExerciseAnalytics(state);
+    Analytics.setCurrentScreen('analysis');
+
+    dispatch({ type: DISMISS_SELECT_EXERCISE });
+};
 
 export const saveSelected1RMExercise = (exercise = 'Squat') => (dispatch, getState) => {
     const state = getState();
+    logSaveExerciseAnalytics(state, exercise);
 
     dispatch(AnalysisActionCreators.saveSelected1RMExercise(exercise));
+};
+
+// ANALYTICS
+
+const logCancelEditExerciseAnalytics = (state) => {
+    Analytics.logEventWithAppState('one_rm_cancel_edit_exercise', {
+    }, state);
+};
+
+const logSaveExerciseAnalytics = (state, exercise) => {
+    Analytics.logEventWithAppState('one_rm_select_exercise', {
+        to_exercise: exercise,
+    }, state);
 };

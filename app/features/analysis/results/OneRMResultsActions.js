79649350ea5129a6f70f0ccec115e7ae27046ce1
JonnyLinja <jonny.linja@gmail.com>
2018-02-25 02:31:16 -0500

    one_rm_exercise_name screen and one_rm_edit_set analytics

--- app/features/analysis/results/OneRMResultsActions.js
+++ app/features/analysis/results/OneRMResultsActions.js
@@ -1,11 +1,22 @@
 import {
     PRESENT_EDIT_1RM_SET,
 } from 'app/configs+constants/ActionTypes';
+import * as Analytics from 'app/services/Analytics';
 
-export const tappedSet = (setID, workoutID) => {
+export const tappedSet = (setID, workoutID) => (dispatch, getState) => {
+    const state = getState();
+    Analytics.setCurrentScreen('one_rm_exercise_name');
+    logEditSetAnalytics(state, setID);
+
     return {
         type: PRESENT_EDIT_1RM_SET,
         setID: setID,
         workoutID: workoutID,
     };
+};
+
+const logEditSetAnalytics = (state, setID) => {
+    Analytics.logEventWithAppState('one_rm_edit_set', {
+        set_id: setID
+    }, state);
 };

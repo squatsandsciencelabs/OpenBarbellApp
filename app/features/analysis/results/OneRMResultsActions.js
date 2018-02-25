b572953dc8d1d37b39fbb9c7d21f9fc3d03f1668
JonnyLinja <jonny.linja@gmail.com>
2018-02-25 02:38:28 -0500

    Fixed tap 1rm point not editing

--- app/features/analysis/results/OneRMResultsActions.js
+++ app/features/analysis/results/OneRMResultsActions.js
@@ -8,11 +8,11 @@ export const tappedSet = (setID, workoutID) => (dispatch, getState) => {
     Analytics.setCurrentScreen('one_rm_exercise_name');
     logEditSetAnalytics(state, setID);
 
-    return {
+    dispatch({
         type: PRESENT_EDIT_1RM_SET,
         setID: setID,
         workoutID: workoutID,
-    };
+    });
 };
 
 const logEditSetAnalytics = (state, setID) => {

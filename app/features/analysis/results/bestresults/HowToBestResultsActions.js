7db6441c5e59ce5c1107a218f93ded500f83ef30
JonnyLinja <jonny.linja@gmail.com>
2018-02-25 03:21:40 -0500

    Best results analytics now works in both places

--- app/features/analysis/results/bestresults/HowToBestResultsActions.js
+++ app/features/analysis/results/bestresults/HowToBestResultsActions.js
@@ -1,21 +1,3 @@
-import {
-    Linking,
-} from 'react-native';
+import * as AnalysisActionCreators from 'app/redux/shared_actions/AnalysisActionCreators';
 
-import {
-    PRESENT_BEST_RESULTS,
-} from 'app/configs+constants/ActionTypes';
-import * as Analytics from 'app/services/Analytics';
-
-export const presentBestResults = () => (dispatch, getState) => {
-    // TODO: analytics
-    const state = getState();
-    logBestResultsAnalytics(state);
-    Linking.openURL("http://www.squatsandscience.com/onerepmax/");
-    dispatch({ type: PRESENT_BEST_RESULTS });
-};
-
-const logBestResultsAnalytics = (state) => {
-    Analytics.logEventWithAppState('one_rm_best_results', {
-    }, state);
-};
+export const presentBestResults = () => AnalysisActionCreators.presentBestResults();
--- app/redux/shared_actions/AnalysisActionCreators.js
+++ app/redux/shared_actions/AnalysisActionCreators.js
@@ -1,8 +1,29 @@
+import {
+    Linking,
+} from 'react-native';
+
 import { 
     SAVE_1RM_EXERCISE,
+    PRESENT_BEST_RESULTS,
 } from 'app/configs+constants/ActionTypes';
+import * as Analytics from 'app/services/Analytics';
 
 export const saveSelected1RMExercise = (exercise = 'Squat') => ({
     type: SAVE_1RM_EXERCISE,
     exercise: exercise,
 });
+
+export const presentBestResults = () => (dispatch, getState) => {
+    const state = getState();
+    logBestResultsAnalytics(state);
+    
+    Linking.openURL("http://www.squatsandscience.com/onerepmax/");
+    dispatch({ type: PRESENT_BEST_RESULTS });
+};
+
+// ANALYTICS
+
+const logBestResultsAnalytics = (state) => {
+    Analytics.logEventWithAppState('one_rm_best_results', {
+    }, state);
+};

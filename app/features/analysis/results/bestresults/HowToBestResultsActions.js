01b581d5488844ce07c49bf86e1c20512742dc9b
JonnyLinja <jonny.linja@gmail.com>
2018-02-25 02:41:25 -0500

    best results analytics

--- app/features/analysis/results/bestresults/HowToBestResultsActions.js
+++ app/features/analysis/results/bestresults/HowToBestResultsActions.js
@@ -5,9 +5,17 @@ import {
 import {
     PRESENT_BEST_RESULTS,
 } from 'app/configs+constants/ActionTypes';
+import * as Analytics from 'app/services/Analytics';
 
 export const presentBestResults = () => (dispatch, getState) => {
     // TODO: analytics
+    const state = getState();
+    logBestResultsAnalytics(state);
     Linking.openURL("http://www.squatsandscience.com/onerepmax/");
     dispatch({ type: PRESENT_BEST_RESULTS });
+};
+
+const logBestResultsAnalytics = (state) => {
+    Analytics.logEventWithAppState('one_rm_best_results', {
+    }, state);
 };

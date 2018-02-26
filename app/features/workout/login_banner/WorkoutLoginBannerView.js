ac27b9b836891d65b7f5a21449ab4502b7a8cb48
JonnyLinja <jonny.linja@gmail.com>
2018-02-26 04:09:01 -0500

    login banner visually shows up in workout when logged out

---
+++ app/features/workout/login_banner/WorkoutLoginBannerView.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+} from 'react-native';
+
+import { SETTINGS_PANEL_STYLES } from 'app/appearance/styles/GlobalStyles';
+
+class WorkoutLoginBannerView extends Component {
+    render() {
+        return (
+            <TouchableOpacity style={styles.container} onPress={ () => this.props.tappedBanner() } >
+                <Text style={styles.text}>Don't lose all your data - tap to sign in and save it!</Text>
+            </TouchableOpacity>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+        backgroundColor: 'rgba(255, 0, 0, 0.7)',
+    },
+    text: {
+        fontSize: 12,
+        color: 'white',
+        textAlign: 'center',
+    }
+});
+
+
+export default WorkoutLoginBannerView;

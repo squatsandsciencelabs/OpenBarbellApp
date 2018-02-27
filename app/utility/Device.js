b9619da66389bd18a163bcf889a8a09e0b91e688
Malek <mmalek1421@gmail.com>
2018-02-27 18:11:01 -0500

    remove safearea and add iphone x check

--- app/utility/Device.js
+++ app/utility/Device.js
@@ -1,7 +1,15 @@
-import { Dimensions } from 'react-native';
+import { Dimensions, Platform } from 'react-native';
 
 export const isSmallDevice = () => {
     const { width } = Dimensions.get('window');
 
     return (width <= 350);
 };
+
+export const isIphoneX = () => {
+    const { width, height } = Dimensions.get('window');
+  
+    return (
+        Platform.OS === 'ios' && (height === 812 || width === 812)
+    );
+}

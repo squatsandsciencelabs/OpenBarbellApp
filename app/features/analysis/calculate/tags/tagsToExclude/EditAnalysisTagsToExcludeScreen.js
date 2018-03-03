27bbfc56b1dbb4fc02b28e2ae7da88e6a0d2013c
JonnyLinja <jonny.linja@gmail.com>
2018-03-02 19:28:28 -0500

    Reverted to Tags to Exclude

--- app/features/analysis/calculate/tags/tagsToExclude/EditAnalysisTagsToExcludeScreen.js
+++ app/features/analysis/calculate/tags/tagsToExclude/EditAnalysisTagsToExcludeScreen.js
@@ -11,7 +11,7 @@ const mapStateToProps = (state) => {
     const exercise = AnalysisSelectors.getExercise(state);
     
     return {
-        title: 'Tags Must Exclude',
+        title: 'Tags to Exclude',
         placeholder: 'Enter Tag',
         text: '',
         inputs: AnalysisSelectors.getTagsToExclude(state),

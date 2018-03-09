823c3d6f93b685585d2536e1b36a770eefad9756
Malek <mmalek1421@gmail.com>
2018-03-09 18:56:00 -0500

    refactored to use setutils

--- app/features/analysis/results/OneRMResultsActions.js
+++ app/features/analysis/results/OneRMResultsActions.js
@@ -22,7 +22,7 @@ export const tappedSet = (setID) => (dispatch, getState) => {
         origWeight: set.hasOwnProperty('weight') ? set.weight : null,
         origMetric: set.hasOwnProperty('metric') ? set.metric : null,
         origTags: set.hasOwnProperty('tags') ? set.tags : [],
-        origDeletedFlag: set.hasOwnProperty('deleted') ? set.deleted : SetUtils.isDeleted(set),
+        origDeletedFlag: SetUtils.isDeleted(set),
     });
 };
 

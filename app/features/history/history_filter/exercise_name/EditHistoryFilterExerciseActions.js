05a09d087374b22523ef616bc701b990fffb4bb6
Malek <mmalek1421@gmail.com>
2018-03-31 11:19:38 -0400

    added actions and screens for forms

---
+++ app/features/history/history_filter/exercise_name/EditHistoryFilterExerciseActions.js
@@ -0,0 +1,51 @@
+import { DISMISS_HISTORY_EXERCISE } from 'app/configs+constants/ActionTypes';
+import * as SetsActionCreators from 'app/redux/shared_actions/SetsActionCreators';
+import * as SetsSelectors from 'app/redux/selectors/SetsSelectors';
+import * as DurationsSelectors from 'app/redux/selectors/DurationsSelectors';
+import * as Analytics from 'app/services/Analytics';
+
+export const dismissExercise = () => {
+    Analytics.setCurrentScreen('history');
+
+    return {
+        type: DISMISS_HISTORY_EXERCISE
+    }
+};
+
+export const cancelExercise = () => (dispatch, getState) => {
+    var state = getState();
+
+    Analytics.setCurrentScreen('history');
+
+    dispatch({
+        type: DISMISS_HISTORY_EXERCISE
+    });
+};
+
+export const saveExerciseName = (exercise) => (dispatch, getState) => {
+    var state = getState();
+    
+    logSaveExerciseNameAnalytics(setID, exercise, state);
+    
+    dispatch(SetsActionCreators.saveHistoryExerciseName(setID, exercise));
+};
+
+const logSaveExerciseNameAnalytics = (setID, exercise, state) => {
+    let duration = DurationsSelectors.getEditHistoryExerciseDuration(state);
+
+    Analytics.logEventWithAppState('save_exercise_name', {
+        value: duration,
+        duration: duration,
+        is_working_set: false,
+    }, state);
+};
+
+const logCancelEditExerciseNameAnalytics = (setID, state) => {
+    let duration = DurationsSelectors.getEditHistoryExerciseDuration(state);
+
+    Analytics.logEventWithAppState('cancel_edit_exercise_name', {
+        value: duration,
+        duration: duration,
+        is_working_set: false,
+    }, state);
+};
---
+++ app/features/history/history_filter/exercise_name/EditHistoryFilterExerciseScreen.js
@@ -0,0 +1,30 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import EditTextModal from 'app/shared_features/edit_set/EditTextModal';
+import * as SuggestionsSelectors from 'app/redux/selectors/SuggestionsSelectors';
+import * as Actions from './EditHistoryFilterExerciseActions';
+
+const mapStateToProps = (state) => ({
+    title: 'Edit Exercise',
+    placeholder: 'Enter Exercise',
+    text: state.history.editingExerciseName,
+    setID: state.history.editingExerciseSetID,
+    generateSingleInputSuggestions: (input) => { return SuggestionsSelectors.generateExerciseNameSuggestions(state, input) },
+    isModalShowing: state.history.editingExerciseSetID !== null
+});
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({
+        saveSetSingleInput: Actions.saveExerciseName,
+        closeModal: Actions.dismissExercise,
+        cancelModal: Actions.cancelExercise,
+    }, dispatch);
+};
+
+const EditHistoryFilterExerciseScreen = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(EditTextModal);
+
+export default EditHistoryFilterExerciseScreen;

dd7b2af81fc70b39ab82e67895a1fdd4ed98f16a
Malek <mmalek1421@gmail.com>
2018-04-01 14:04:21 -0400

    setting up more reducers

--- app/features/history/history_filter/EditHistoryFilterScreen.js
+++ app/features/history/history_filter/EditHistoryFilterScreen.js
@@ -7,12 +7,24 @@ import * as HistorySelectors from 'app/redux/selectors/HistorySelectors';
 
 // REFACTOR as selector When historyfilter is approved
 const mapStateToProps = (state) => ({
-    isModalShowing: state.history.showHistoryFilter
+    isModalShowing: HistorySelectors.getShowHistoryFilter(state),
+    exercise: HistorySelectors.getHistoryFilterExercise(state),
+    tagsToInclude: HistorySelectors.getHistoryFilterTagsToInclude(state),
+    tagsToExclude: HistorySelectors.getHistoryFilterTagsToExclude(state),
+    startDate: HistorySelectors.getHistoryFilterStartingDate(state),
+    endDate: HistorySelectors.getHistoryFilterEndingDate(state),
+    startWeight: HistorySelectors.getHistoryFilterStartingWeight(state),
+    endWeight: HistorySelectors.getHistoryFilterEndingWeight(state),
+    startingRPE: HistorySelectors.getHistoryFilterStartingRPE(state),
+    endingRPE: HistorySelectors.getHistoryFilterEndingRPE(state),
+    startingRepRange: HistorySelectors.getHistoryFilterStartingRepRange(state),
+    endingRepRange: HistorySelectors.getHistoryFilterEndingRepRange(state),
 });
 
 const mapDispatchToProps = (dispatch) => {
     return bindActionCreators({
         closeModal: Actions.dismissHistoryFilter,
+        tappedExercise: Actions.presentSelectExercise,
     }, dispatch);
 };
 

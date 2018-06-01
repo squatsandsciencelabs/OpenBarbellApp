2fc25acba19eea9f50cca3da98d4155ab0031bc7
Malek <mmalek1421@gmail.com>
2018-06-01 00:24:30 -0400

    warning for start and end dates chronology disorders

--- app/features/history/history_filter/dateRange/endDate/EditHistoryFilterEndDateActions.js
+++ app/features/history/history_filter/dateRange/endDate/EditHistoryFilterEndDateActions.js
@@ -1,12 +1,25 @@
+import { Alert } from 'react-native';
+import moment from 'moment';
+import * as HistorySelectors from 'app/redux/selectors/HistorySelectors';
 import {
     SAVE_HISTORY_FILTER_END_DATE,
     DISMISS_HISTORY_FILTER_END_DATE,
 } from 'app/configs+constants/ActionTypes';
 
-export const changeDate = (date) => ({ 
-    type: SAVE_HISTORY_FILTER_END_DATE,
-    date: date.toString(),
-});
+export const changeDate = (date) => (dispatch, getState) => {
+    const state = getState();
+    const startDate = HistorySelectors.getEditingHistoryFilterStartingDate(state);
+    const endDate = date.toString();
+
+    if (moment(startDate) < moment(endDate) || !startDate) {
+        dispatch({ 
+            type: SAVE_HISTORY_FILTER_END_DATE,
+            date: endDate,
+        });
+    } else {
+        Alert.alert("End date must be after start date")
+    }
+};
 
 export const dismissPicker = () => ({
     type: DISMISS_HISTORY_FILTER_END_DATE,

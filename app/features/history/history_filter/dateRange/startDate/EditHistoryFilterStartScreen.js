685543a3eb344af19c4861bb0e84c34efcf2c5f1
JonnyLinja <jonny.linja@gmail.com>
2018-06-04 17:36:35 -0400

    fix for filter date crashes

--- app/features/history/history_filter/dateRange/startDate/EditHistoryFilterStartScreen.js
+++ app/features/history/history_filter/dateRange/startDate/EditHistoryFilterStartScreen.js
@@ -6,10 +6,17 @@ import * as Actions from './EditHistoryFilterStartDateActions';
 import * as HistorySelectors from 'app/redux/selectors/HistorySelectors';
 import * as DateUtils from 'app/utility/DateUtils';
 
-const mapStateToProps = (state) => ({
-    date: DateUtils.getDate(HistorySelectors.getEditingHistoryFilterStartingDate(state)),
-    isVisible: HistorySelectors.getIsEditingHistoryFilterStartingDate(state),
-});
+const mapStateToProps = (state) => {
+    let date = DateUtils.getDate(HistorySelectors.getEditingHistoryFilterStartingDate(state));
+    if (!date) {
+        date = new Date();
+    }
+    
+    return {
+        date,
+        isVisible: HistorySelectors.getIsEditingHistoryFilterStartingDate(state),
+    };
+};
 
 const mapDispatchToProps = (dispatch) => {
     return bindActionCreators({

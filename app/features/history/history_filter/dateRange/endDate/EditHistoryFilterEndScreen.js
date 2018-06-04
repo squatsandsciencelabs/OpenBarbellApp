2d1e6f77726c50e1ffa703db7dbe61e8b140c2ca
JonnyLinja <jonny.linja@gmail.com>
2018-06-04 17:24:37 -0400

    filter dates now start from previous date val

--- app/features/history/history_filter/dateRange/endDate/EditHistoryFilterEndScreen.js
+++ app/features/history/history_filter/dateRange/endDate/EditHistoryFilterEndScreen.js
@@ -1,11 +1,13 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-
 import DatePicker from 'app/shared_features/date_picker/DatePicker';
+
 import * as Actions from './EditHistoryFilterEndDateActions';
 import * as HistorySelectors from 'app/redux/selectors/HistorySelectors';
+import * as DateUtils from 'app/utility/DateUtils';
 
 const mapStateToProps = (state) => ({
+    date: DateUtils.getDate(HistorySelectors.getEditingHistoryFilterEndingDate(state)),
     isVisible: HistorySelectors.getIsEditingHistoryFilterEndingDate(state),
 });
 
--- app/features/history/history_filter/dateRange/startDate/EditHistoryFilterStartScreen.js
+++ app/features/history/history_filter/dateRange/startDate/EditHistoryFilterStartScreen.js
@@ -1,11 +1,13 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-
 import DatePicker from 'app/shared_features/date_picker/DatePicker';
+
 import * as Actions from './EditHistoryFilterStartDateActions';
 import * as HistorySelectors from 'app/redux/selectors/HistorySelectors';
+import * as DateUtils from 'app/utility/DateUtils';
 
 const mapStateToProps = (state) => ({
+    date: DateUtils.getDate(HistorySelectors.getEditingHistoryFilterStartingDate(state)),
     isVisible: HistorySelectors.getIsEditingHistoryFilterStartingDate(state),
 });
 

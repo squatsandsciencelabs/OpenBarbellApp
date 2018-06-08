89500005826707d0a8833f2f427771fee324ed65
Malek <mmalek1421@gmail.com>
2018-06-07 22:34:20 -0400

    actions for velocity drop actions

--- app/redux/sagas/VelocityDropSaga.js
+++ app/redux/sagas/VelocityDropSaga.js
@@ -1,12 +1,16 @@
 import { takeEvery, select, put, call, all } from 'redux-saga/effects';
-import { PLAY_VELOCITY_DROP_AUDIO } from 'app/configs+constants/ActionTypes';
+import { 
+    VELOCITY_DROPPED, 
+    PLAY_VELOCITY_DROP_AUDIO 
+} from 'app/configs+constants/ActionTypes';
+import * as VelocityDropActionCreators from 'app/redux/shared_actions/VelocityDropActionCreators';
 
 const VelocityDropSaga = function * VelocityDropSaga() {
-    yield takeEvery(PLAY_VELOCITY_DROP_AUDIO, playVelocityDropAudio);
+    yield takeEvery(VELOCITY_DROPPED, playVelocityDropAudio);
 };
 
 function* playVelocityDropAudio() {
-    console.tron.log("VELOCITY DROPPED");
+    yield put(VelocityDropActionCreators.playVelocityDropAudio());
 };
 
 export default VelocityDropSaga;

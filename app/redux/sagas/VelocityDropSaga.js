7da5d3b9088ab8ffc08d179825d2fb8ef2201385
Malek <mmalek1421@gmail.com>
2018-06-10 23:05:04 -0400

    moved into sagas

--- app/redux/sagas/VelocityDropSaga.js
+++ app/redux/sagas/VelocityDropSaga.js
@@ -1,16 +1,28 @@
-import { takeEvery, select, put, call, all } from 'redux-saga/effects';
+import { takeLatest, select, put, call, all } from 'redux-saga/effects';
 import { 
-    VELOCITY_DROPPED, 
-    PLAY_VELOCITY_DROP_AUDIO 
+    ADD_REP_DATA, 
 } from 'app/configs+constants/ActionTypes';
+import * as SetsSelectors from 'app/redux/selectors/SetsSelectors';
+import * as RepDataMap from 'app/utility/RepDataMap';
 import * as VelocityDropActionCreators from 'app/redux/shared_actions/VelocityDropActionCreators';
+import * as SettingsSelectors from 'app/redux/selectors/SettingsSelectors';
 
 const VelocityDropSaga = function * VelocityDropSaga() {
-    yield takeEvery(VELOCITY_DROPPED, playVelocityDropAudio);
+    yield takeLatest(ADD_REP_DATA, playVelocityDropAudio);
 };
 
-function* playVelocityDropAudio() {
-    yield put(VelocityDropActionCreators.playVelocityDropAudio());
+function* playVelocityDropAudio(action) {
+    const velocityThreshold = yield select(SettingsSelectors.getVelocityDropThreshold);
+    const workingSet = yield select(SetsSelectors.getWorkingSet);
+    const workingSetReps = workingSet.reps
+    const lastRep = workingSetReps[workingSetReps.length - 2];
+    const currentRep = RepDataMap.averageVelocity(action.data);
+    const lastRepVelocity = lastRep ? RepDataMap.averageVelocity(lastRep.data) : null;
+
+    if (lastRepVelocity && (lastRepVelocity * (1 - velocityThreshold)) > currentRep) {
+        console.tron.log(currentRep + " dropped from " + lastRepVelocity);
+        yield put(VelocityDropActionCreators.playVelocityDropAudio());
+    }
 };
 
 export default VelocityDropSaga;
